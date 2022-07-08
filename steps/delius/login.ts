d762b3ea2665048368cef41a901a874c0abdcee8
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2022-07-08 11:52:42 +0100

    Add allocation to probation practitioner

--- steps/delius/login.ts
+++ steps/delius/login.ts
@@ -1,13 +1,13 @@
-import {Page, expect} from '@playwright/test';
+import { Page, expect } from '@playwright/test'
 // @ts-ignore
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
 export const login = async (page: Page) => {
-    dotenv.config();
-    await page.goto(process.env.DELIUS_URL);
-    await expect(page).toHaveTitle(/HMPPS Digital Services - Sign in/);
-    await page.fill('id=username', process.env.DELIUS_USERNAME!!);
-    await page.fill('id=password', process.env.DELIUS_PASSWORD!!);
-    await page.click('id=submit');
-    await expect(page).toHaveTitle(/National Delius Home Page/);
+    dotenv.config()
+    await page.goto(process.env.DELIUS_URL)
+    await expect(page).toHaveTitle(/HMPPS Digital Services - Sign in/)
+    await page.fill('id=username', process.env.DELIUS_USERNAME!!)
+    await page.fill('id=password', process.env.DELIUS_PASSWORD!!)
+    await page.click('id=submit')
+    await expect(page).toHaveTitle(/National Delius Home Page/)
 }
--- tests/allocations/allocate-new-person.spec.ts
+++ tests/allocations/allocate-new-person.spec.ts
@@ -1,16 +1,21 @@
-import {test} from '@playwright/test';
-import {login} from "../../steps/delius/login";
-import {createOffender} from "../../steps/delius/offender/create-offender";
-import {createEventForCRN} from "../../steps/delius/event/create-event";
-import {createRequirementForEvent} from "../../steps/delius/requirement/create-requirement";
+import { test } from '@playwright/test'
+import { login } from '../../steps/delius/login'
+import { createOffender } from '../../steps/delius/offender/create-offender'
+import { createEventForCRN } from '../../steps/delius/event/create-event'
+import { createRequirementForEvent } from '../../steps/delius/requirement/create-requirement'
+import { login as workforceLogin } from '../../steps/workforce/login'
+import { viewAllocation, allocateCase } from '../../steps/workforce/viewAllocation'
 
+test.beforeEach(async ({ page }) => {
+    await login(page)
+})
 
-test.beforeEach(async ({page}) => {
-    await login(page);
-});
-
-test('Create offender with event and requirement', async ({page}) => {
+test('Create offender with event and requirement', async ({ page }) => {
     const crn = await createOffender(page)
     await createEventForCRN(page, crn)
     await createRequirementForEvent(page, crn, '1')
-});
+
+    await workforceLogin(page)
+    await viewAllocation(page, crn)
+    await allocateCase(page, crn)
+})
