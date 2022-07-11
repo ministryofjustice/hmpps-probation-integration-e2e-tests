bbe6f2788eb8c49b196746bd0b7164d7f9ac0cc1
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-07-11 17:12:03 +0100

    verifying allocation in ND

--- tests/allocations/allocate-new-person.spec.ts
+++ tests/allocations/allocate-new-person.spec.ts
@@ -16,27 +16,27 @@ const wrexhamTeam = "NPS - Wrexham - Team 1"
 const practitioner = {firstName: "Carlo", lastName: "Veo"}
 
 test("Allocate new offender with community event and requirement", async ({page}) => {
-    // const crn = await createOffender(page, {providerName: npsWales})
-    // await createEventForCRN(page, {
-    //     crn,
-    //     providerName: npsWales,
-    //     teamName: wrexhamTeam,
-    //     appearanceType: "Sentence",
-    //     outcome: "ORA Community Order",
-    //     length: "6"
-    // })
-    //
-    // await createRequirementForEvent(page, {
-    //     crn,
-    //     eventNumber: "1",
-    //     providerName: npsWales,
-    //     teamName: wrexhamTeam,
-    //     category: "Curfew",
-    //     subCategory: "Curfew",
-    //     length: "6"
-    // })
-    //
-    // await workforceLogin(page)
-    // await allocateCase(page, crn, practitioner)
-    await verifyAllocation(page, {crn:'X525622', practitioner})
+    const crn = await createOffender(page, {providerName: npsWales})
+    await createEventForCRN(page, {
+        crn,
+        providerName: npsWales,
+        teamName: wrexhamTeam,
+        appearanceType: "Sentence",
+        outcome: "ORA Community Order",
+        length: "6"
+    })
+
+    await createRequirementForEvent(page, {
+        crn,
+        eventNumber: "1",
+        providerName: npsWales,
+        teamName: wrexhamTeam,
+        category: "Curfew",
+        subCategory: "Curfew",
+        length: "6"
+    })
+
+    await workforceLogin(page)
+    await allocateCase(page, crn, practitioner)
+    await verifyAllocation(page, {crn, practitioner})
 })
