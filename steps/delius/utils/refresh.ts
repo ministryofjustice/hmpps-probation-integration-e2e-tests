0c0911c7f9b7e0888eddc933e99f2a358abb5835
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-07-25 15:50:25 +0100

    added doUntil method

--- steps/delius/utils/refresh.ts
+++ steps/delius/utils/refresh.ts
@@ -1,8 +1,15 @@
 import {Page} from "@playwright/test";
 
-export const refreshUntil = async (page: Page, predicate: ()=>Promise<boolean>, timeout: number = 20) => {
+export const refreshUntil = async (page: Page, predicate: () => Promise<boolean>, timeout: number = 20) => {
     const waitUntil = new Date().getSeconds() + timeout
     while (!await predicate() && new Date().getSeconds() <= waitUntil) {
         await page.reload({waitUntil: "domcontentloaded"})
+    }
+}
+
+export const doUntil = async (page: Page, predicate: () => Promise<boolean>, action: () => Promise<void>, timeout: number = 20) => {
+    const waitUntil = new Date().getSeconds() + timeout
+    while (!await predicate() && new Date().getSeconds() <= waitUntil) {
+        await action()
     }
 }
