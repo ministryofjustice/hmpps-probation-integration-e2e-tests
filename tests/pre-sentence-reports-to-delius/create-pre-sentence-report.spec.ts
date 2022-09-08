974b4ce70088fb4fa65c9ee4eac4be4c08f50a57
Marcus Aspin <maspin@unilink.com>
2022-09-08 13:00:45 +0100

    PI-409 Fill in the remaining pre-sentence report fields

--- tests/pre-sentence-reports-to-delius/create-pre-sentence-report.spec.ts
+++ tests/pre-sentence-reports-to-delius/create-pre-sentence-report.spec.ts
@@ -29,7 +29,7 @@ test('Create a short format pre-sentence report', async ({ page }) => {
     await expect(popup).toHaveTitle(/Short Format Pre-Sentence Report - Offender details/)
     await popup.locator('textarea[name="address"]').fill(faker.address.streetAddress(true))
     await popup.locator('input[name="pnc"]').fill(faker.random.alphaNumeric(10, { casing: 'upper' }))
-    await popup.locator('text=Save and continue').click() // FIXME this currently fails due to an error in pre-sentence-service
+    await popup.locator('text=Save and continue').click()
     // - Sentencing court details
     await expect(popup).toHaveTitle(/Short Format Pre-Sentence Report - Sentencing court details/)
     const dateOfHearing = faker.date.recent()
@@ -39,16 +39,44 @@ test('Create a short format pre-sentence report', async ({ page }) => {
     await popup.locator('text=Save and continue').click()
     // - Offence details
     await expect(popup).toHaveTitle(/Short Format Pre-Sentence Report - Offence details/)
-    for (const textarea of await popup.locator('textarea').elementHandles()) {
-        await textarea.fill(faker.lorem.paragraphs())
+    for (const textarea of await popup.locator('[contenteditable]').elementHandles()) {
+        await textarea.click()
+        await popup.keyboard.type(faker.lorem.sentence())
     }
     await popup.locator('text=Save and continue').click()
     // - Offence analysis
     await expect(popup).toHaveTitle(/Short Format Pre-Sentence Report - Offence analysis/)
-    for (const textarea of await popup.locator('textarea').elementHandles()) {
-        await textarea.fill(faker.lorem.paragraphs())
+    for (const textarea of await popup.locator('[contenteditable]').elementHandles()) {
+        await textarea.click()
+        await popup.keyboard.type(faker.lorem.sentence())
     }
-    // ... etc
+    await popup.locator('text=Save and continue').click()
+    // - Offender assessment
+    await expect(popup).toHaveTitle(/Short Format Pre-Sentence Report - Offender assessment/)
+    await popup.locator('input[name=experienceOfTrauma][value=no]').click()
+    await popup.locator('input[name=caringResponsibilities][value=no]').click()
+    await popup.locator('text=Save and continue').click()
+    // - Risk assessment
+    await expect(popup).toHaveTitle(/Short Format Pre-Sentence Report - Risk assessment/)
+    for (const textarea of await popup.locator('[contenteditable]').elementHandles()) {
+        await textarea.click()
+        await popup.keyboard.type(faker.lorem.sentence())
+    }
+    await popup.locator('input[type=radio][value="N/A"]').click()
+    await popup.locator('text=Save and continue').click()
+    // - Proposal
+    await expect(popup).toHaveTitle(/Short Format Pre-Sentence Report - Proposal/)
+    await popup.locator('input[type=radio][value=yes]').click()
+    for (const textarea of await popup.locator('[contenteditable]').elementHandles()) {
+        await textarea.click()
+        await popup.keyboard.type(faker.lorem.sentence())
+    }
+    await popup.locator('text=Save and continue').click()
+    // - Sources of information
+    await expect(popup).toHaveTitle(/Short Format Pre-Sentence Report - Sources of information/)
+    await popup.locator('input[type=checkbox][value=interviewInformationSource]').click()
+    await popup.locator('text=Save and continue').click()
+    // TODO Sign the report. This isn't working atm in the pre-sentence service, so the report will remain as a draft and cause the test to fail further down.
     await popup.close()
 
     // Then the document appears in the Delius document list
