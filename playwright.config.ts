afe4a67d4978ac966455be7a8bff2f9647f76cc6
Marcus Aspin <maspin@unilink.com>
2022-10-03 14:18:54 +0100

    Reduce global test timeout, and enable parallel tests on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,15 +13,13 @@ import { devices } from '@playwright/test'
 const config: PlaywrightTestConfig = {
     testDir: './tests',
     /* Maximum time one test can run for. */
-    timeout: 480 * 1000,
+    timeout: 180 * 1000,
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
