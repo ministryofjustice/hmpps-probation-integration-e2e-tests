f8b2da1a60610756eb796a6fa6165cf4e3760dca
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-10-12 08:58:31 +0100

    PI-476

--- steps/delius/contact/create-contact.ts
+++ steps/delius/contact/create-contact.ts
@@ -3,24 +3,43 @@ import { findContactsByCRN } from './find-contacts'
 import { fillTime, selectOption } from '../utils/inputs'
 import { waitForAjax } from '../utils/refresh'
 
-export const createContact = async (page: Page, crn: string) => {
-    await findContactsByCRN(page, crn)
+export interface ContactOptions {
+    crn: string,
+    relatedTo: string,
+    category: string
+    type: string,
+    team: string,
+    staff: string
+}
+
+export const createContact = async (page: Page, options: ContactOptions) => {
+    await findContactsByCRN(page, options.crn)
     await page.locator('input.btn', { hasText: 'Add Contact' }).first().click()
     await expect(page).toHaveTitle('Add Contact Details')
-    await selectOption(page, '#addContactForm\\:RelatedTo', 'Event 1 - ORA Community Order (6 Months)')
-    await Promise.all([selectOption(page, '#addContactForm\\:ContactCategory', 'All/Always'), waitForAjax(page)])
+
+    await selectOption(page, '#addContactForm\\:RelatedTo', options.relatedTo)
+    await Promise.all([selectOption(page, '#addContactForm\\:ContactCategory', options.category), waitForAjax(page)])
     await Promise.all([
-        selectOption(page, '#addContactForm\\:ContactType', 'Initial Appointment (NS)'),
+        selectOption(page, '#addContactForm\\:ContactType', options.type),
         waitForAjax(page),
     ])
     await fillTime(page, '#addContactForm\\:StartTime', new Date())
     await fillTime(page, '#addContactForm\\:EndTime', new Date(new Date().getTime() + 60000))
     await Promise.all([
-        selectOption(page, '#addContactForm\\:TransferToTeam', 'Unallocated Team(N03)'),
+        selectOption(page, '#addContactForm\\:TransferToTeam', options.team),
         waitForAjax(page),
     ])
     await Promise.all([selectOption(page, '#addContactForm\\:Location'), waitForAjax(page)])
-    await Promise.all([selectOption(page, '#addContactForm\\:TransferToOfficer', 'Unallocated'), waitForAjax(page)])
+    await Promise.all([selectOption(page, '#addContactForm\\:TransferToOfficer', options.staff), waitForAjax(page)])
     await page.locator('#addContactForm\\:saveButton').click()
     await expect(page).toHaveTitle(/Contact List/)
 }
+
+export const createInitialAppointment = async (page: Page, crn: string, eventNumber: string) =>  createContact(page, {
+    crn: crn,
+    relatedTo: `Event ${eventNumber} - ORA Community Order (6 Months)`,
+    category: 'All/Always',
+    type: 'Initial Appointment (NS)',
+    team: 'Unallocated Team(N03)',
+    staff: 'Unallocated'
+})
