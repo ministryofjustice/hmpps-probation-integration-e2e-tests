904b250bca8b754b4d173fc88c1ac5b5c171175d
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2022-10-20 10:18:14 +0100

    Pi 478 randm end to end tests (#64)
    * PI-478 Add basic end-to-end test for Refer and Monitor integration

---
+++ steps/delius/event/find-nsi.ts
@@ -0,0 +1,16 @@
+import { expect, Page } from '@playwright/test'
+import { findOffenderByCRN } from '../offender/find-offender'
+import { findEventByCRN } from './find-events'
+
+export async function findNSIByCRN(page: Page, crn: string, eventNumber: number, NSIText: string) {
+    // Search for event
+    await findOffenderByCRN(page, crn)
+    await findEventByCRN(page, crn, eventNumber)
+
+    // Find the correct NSI in the list
+    await page.click('#linkNavigation3EventNsi')
+    await expect(page).toHaveTitle(/Non Statutory Intervention List/)
+    await page.locator('main[role="main"]').locator('a', { hasText: 'view' }).click()
+    await expect(page).toHaveTitle(/Non Statutory Intervention Details/)
+    await expect(page.locator("div:right-of(:text('NSI Provider:'))", { hasText: NSIText })).not.toHaveCount(0)
+}
