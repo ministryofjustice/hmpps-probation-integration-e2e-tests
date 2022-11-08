d46c8120bb143733ce58098849fb68337097fe74
Marcus Aspin <maspin@unilink.com>
2022-11-08 09:06:02 +0000

    PI-435 Fix RSR score selector (#75)

--- steps/delius/oasys-rsr-score/verify-rsr-score.ts
+++ steps/delius/oasys-rsr-score/verify-rsr-score.ts
@@ -1,8 +1,9 @@
 import { expect, type Page } from '@playwright/test'
+import { refreshUntil } from '../utils/refresh.js'
 
 export const verifyRsrScore = async (page: Page, score: string) => {
     await page.click('#linkNavigation2EventList')
     await page.click("a[title='View event']")
     await page.click('#linkNavigation3CaseAllocation')
-    await expect(page.locator('#interAreaForm:RSRScore')).toHaveText(score)
+    await refreshUntil(page, () => expect(page.locator('#interAreaForm\\:RSRScore')).toHaveText(score))
 }
