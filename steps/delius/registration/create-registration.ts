5a6552fd15d0e22a5520a09cdf9faace2375de3e
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-12-07 12:37:56 +0000

    added wait for ajax to allow population of teams/staff correctly and then second click on button as ND not registering first one

--- steps/delius/registration/create-registration.ts
+++ steps/delius/registration/create-registration.ts
@@ -2,6 +2,7 @@ import { type Page } from '@playwright/test'
 import { expect } from '@playwright/test'
 import { selectOption } from '../utils/inputs.js'
 import { findOffenderByCRN } from '../offender/find-offender.js'
+import { waitForAjax } from "../utils/refresh.js";
 
 export async function createRegistration(page: Page, crn: string, registrationType: string) {
     await findOffenderByCRN(page, crn)
@@ -11,9 +12,15 @@ export async function createRegistration(page: Page, crn: string, registrationTy
     await page.locator('input', { hasText: 'Add Registration' }).click()
     await expect(page).toHaveTitle('Add Registration')
     await selectOption(page, '#addRegistrationForm\\:Trust', 'NPS London')
+    await waitForAjax(page)
     await selectOption(page, '#addRegistrationForm\\:RegisterType', registrationType)
+    await waitForAjax(page)
     await selectOption(page, '#addRegistrationForm\\:Team')
+    await waitForAjax(page)
     await selectOption(page, '#addRegistrationForm\\:Staff')
-    await page.locator('input', { hasText: 'Save' }).click()
+    const saveBtn = page.locator('input', { hasText: 'Save' })
+    await saveBtn.click()
+    //click a second time as ND screen refreshes and doesn't register first click
+    await saveBtn.click()
     await expect(page.locator('tbody tr')).toContainText(registrationType)
 }
