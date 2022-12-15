db7793621f1ffb6c69974d131684a2ae133b25eb
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-12-15 09:58:51 +0000

    OLE noms

--- steps/delius/offender/update-offender.ts
+++ steps/delius/offender/update-offender.ts
@@ -1,6 +1,7 @@
 import { expect, type Page } from '@playwright/test'
 import { findOffenderByCRN } from './find-offender.js'
 import { selectOption } from '../utils/inputs.js'
+import {refreshUntil} from "../utils/refresh";
 
 export async function setNomisId(page: Page, crn: string, nomisId: string) {
     await findOffenderByCRN(page, crn)
@@ -13,5 +14,15 @@ export async function setNomisId(page: Page, crn: string, nomisId: string) {
     await page.fill('#updateOffenderForm\\:identifierValue', nomisId)
     await page.locator('input', { hasText: 'Add/Update' }).click()
     await page.locator('input', { hasText: 'Save' }).click()
-    await expect(page).toHaveTitle(/Personal Details/)
+    
+    try {
+        await expect(page).toHaveTitle(/Personal Details/)
+    } catch (e) {
+        const ole = await page.locator("span.text-danger", {
+            hasText: "Entity could not be updated - it has been modified in another transaction since it was loaded. [DAOERR002]"
+        })
+        if (ole != null) {
+            await setNomisId(page, crn, nomisId)
+        }
+    }
 }
