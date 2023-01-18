20097d0a92f54accc1fd039e264dc0b00e5ff9e1
Marcus Aspin <maspin@unilink.com>
2023-01-18 12:35:38 +0000

    PI-689 Stub test for make-recall-decisions-and-delius

---
+++ tests/make-recall-decisions-and-delius/make-recall-decision.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test'
+
+test('stub', async () => {
+    // TODO add test steps here, e.g. making a recall recommendation then verifying the contacts in Delius.
+    //  Note: testing has been completed manually for now, and deployment to production has been approved.
+    expect(true).toBeTruthy()
+})
