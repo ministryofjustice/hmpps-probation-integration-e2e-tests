5dada119a816da7f56cf314d547086c92cac07be
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-01-27 12:31:02 +0000

    PI-746 Added e2e test for temporary absence return

--- steps/delius/release/create-release.ts
+++ steps/delius/release/create-release.ts
@@ -1,17 +1,26 @@
 import { expect, type Page } from '@playwright/test'
-import { selectOption } from '../utils/inputs.js'
+import {fillDate, selectOption} from '../utils/inputs.js'
 import { findEventByCRN } from '../event/find-events.js'
-import { DeliusDateFormatter, Yesterday } from '../utils/date-time.js'
+import {DeliusDateFormatter, Tomorrow, Yesterday} from '../utils/date-time.js'
 
-export const createRelease = async (page: Page, crn: string, eventNumber = 1) => {
+export const createRelease = async (page: Page, crn: string, eventNumber = 1, temporary: boolean = false) => {
     await findEventByCRN(page, crn, eventNumber)
     await page.getByRole('button', { name: 'Throughcare' }).click()
     await expect(page.locator('h1')).toHaveText('Throughcare Details')
     await page.getByRole('button', { name: 'Add Release' }).click()
     await expect(page.locator('h1')).toHaveText('Add Release')
     await page.getByLabel(/Actual Release Date/).fill(DeliusDateFormatter(Yesterday))
-    await page.getByLabel(/Release Type/).selectOption('Adult Licence')
+    if (temporary) {
+        await page.getByLabel(/Release Type/).selectOption('Release on Temporary Licence')
+    }
+    else {
+        await page.getByLabel(/Release Type/).selectOption('Adult Licence')
+    }
     await selectOption(page, '#addRelease\\:Institution')
+    if (temporary) {
+        await page.getByLabel(/Release on Licence Length/).fill("1")
+        await page.getByLabel(/Release on Licence End Date/).fill(DeliusDateFormatter(new Date()))
+    }
     await page.getByRole('button', { name: 'Save' }).click()
     await page.getByRole('button', { name: 'Confirm' }).click()
     await expect(page.locator('h1')).toHaveText('Add Licence Conditions')
--- tests/prison-custody-status-to-delius/release-recall-test.spec.ts
+++ tests/prison-custody-status-to-delius/release-recall-test.spec.ts
@@ -3,9 +3,16 @@ import { login as deliusLogin } from '../../steps/delius/login.js'
 import { createOffender } from '../../steps/delius/offender/create-offender.js'
 import { createCustodialEvent } from '../../steps/delius/event/create-event.js'
 import { deliusPerson } from '../../steps/delius/utils/person.js'
-import { createAndBookPrisoner, recallPrisoner, releasePrisoner } from '../../steps/api/dps/prison-api.js'
+import {
+    createAndBookPrisoner,
+    recallPrisoner,
+    releasePrisoner, temporaryAbsenceReturn,
+    temporaryReleasePrisoner
+} from '../../steps/api/dps/prison-api.js'
 import { findCustodyForEventByCRN } from '../../steps/delius/event/find-events.js'
 import { refreshUntil } from '../../steps/delius/utils/refresh.js'
+import { createRelease } from "../../steps/delius/release/create-release.js";
+import { createLicenceCondition } from "../../steps/delius/licence-condition/create-licence-condition.js";
 
 const nomisIds = []
 
@@ -39,6 +46,41 @@ test('Release and recall test', async ({ page }) => {
 
     // When the person in nomis is recalled
     await recallPrisoner(nomisId)
+    // Then the person is recalled in Delius
+    await refreshUntil(
+        page,
+        () => expect(page.locator("//span[contains(text(),'Moorland (HMP & YOI)')]")).toHaveCount(3),
+        { timeout: 180_000 }
+    )
+})
+
+test('Temporary absence test', async ({ page }) => {
+    test.slow() // increase the timeout - releases/recall publishing can take a few minutes
+
+    // Given a person with a sentenced event in Delius
+    await deliusLogin(page)
+    // await hmppsLogin(page)
+    const person = deliusPerson()
+    const crn = await createOffender(page, { person })
+    await createCustodialEvent(page, { crn })
+
+    // And a corresponding person and booking in NOMIS
+    const { nomisId } = await createAndBookPrisoner(page, crn, person)
+    nomisIds.push(nomisId)
+
+    await findCustodyForEventByCRN(page, crn, 1)
+    await refreshUntil(
+        page,
+        () => expect(page.locator("//span[contains(text(),'Moorland (HMP & YOI)')]")).toHaveCount(3),
+        { timeout: 180_000 }
+    )
+
+    await temporaryReleasePrisoner(nomisId)
+    await createRelease(page, crn, 1, true)
+    await createLicenceCondition(page, crn, 1)
+
+    // When the person returns to custody
+    await temporaryAbsenceReturn(nomisId)
     // Then the person is recalled in Delius
     await refreshUntil(
         page,
