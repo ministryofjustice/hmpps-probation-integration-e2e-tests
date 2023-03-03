048916e1d87ffb8307bca5e10ee7ba107449a4e9
smalepati99 <smalepati99@yahoo.com>
2023-03-03 17:02:13 +0000

    Added automation tests incorporating the recent changes by Approved Premises Delius ==> OASys Layer3 ==> AP Applications ==> Approved Premises - Create a Booking ==> Delius verification

--- steps/approved-premises/applications/add-details-managing-risks-needs.ts
+++ steps/approved-premises/applications/add-details-managing-risks-needs.ts
@@ -7,8 +7,6 @@ export const addRisksNeedsDetails = async (page: Page) => {
     //"Has Offender ever been convicted of the following offences" Page
     await expect(page.locator('#main-content h1')).toHaveText(/Has \w+ \w+ ever been convicted of the following offences?/)
     await page.getByLabel('No').check()
-    // await page.locator('button', { hasText: 'Save and continue' }).click();
-    // await page.locator('.govuk-button').click();
     await page.locator('.govuk-button', { hasText: 'Save and continue' }).click();
     //"Which of the rehabilitative activities will assist the person's rehabilitation in the Approved Premises (AP)?" Page
     await expect(page.locator('#main-content h1')).toHaveText("Which of the rehabilitative activities will assist the person's rehabilitation in the Approved Premises (AP)?")
