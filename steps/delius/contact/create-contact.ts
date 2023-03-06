6ce42ab437d10f2a0d3cc0605deabd8957ca48d1
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-03-06 16:19:56 +0000

    PI-834 (#178)

--- steps/delius/contact/create-contact.ts
+++ steps/delius/contact/create-contact.ts
@@ -1,6 +1,6 @@
 import { expect, type Page } from '@playwright/test'
 import { findContactsByCRN } from './find-contacts.js'
-import { fillTime, selectOption, selectOptionAndWait } from '../utils/inputs.js'
+import { fillDate, fillTime, selectOptionAndWait } from '../utils/inputs.js'
 import { Contact, data, Team } from '../../../test-data/test-data.js'
 import { doUntil } from '../utils/refresh.js'
 
@@ -8,14 +8,21 @@ export const createContact = async (page: Page, crn: string, options: Contact) =
     await findContactsByCRN(page, crn)
     await page.locator('input.btn', { hasText: 'Add Contact' }).first().click()
     await expect(page).toHaveTitle('Add Contact Details')
+    if (options.date) {
+        await fillDate(page, '#addContactForm\\:StartDate', options.date)
+    }
     await selectOptionAndWait(page, '#addContactForm\\:RelatedTo', options.relatesTo)
     await selectOptionAndWait(page, '#addContactForm\\:TransferToTrust', options.allocation?.team?.provider)
     await selectOptionAndWait(page, '#addContactForm\\:ContactCategory', options.category)
     await selectOptionAndWait(page, '#addContactForm\\:TransferToTeam', options.allocation?.team?.name)
     await selectOptionAndWait(page, '#addContactForm\\:ContactType', options.type)
     await selectOptionAndWait(page, '#addContactForm\\:Location', options.allocation?.team?.location)
-    await fillTime(page, '#addContactForm\\:StartTime', new Date())
-    await fillTime(page, '#addContactForm\\:EndTime', new Date(new Date().getTime() + 60000))
+    if (options.startTime) {
+        await fillTime(page, '#addContactForm\\:StartTime', options.startTime)
+    }
+    if (options.endTime) {
+        await fillTime(page, '#addContactForm\\:EndTime', options.endTime)
+    }
     await selectOptionAndWait(page, '#addContactForm\\:TransferToOfficer', options.allocation?.staff?.name)
     await doUntil(
         () => page.locator('#addContactForm\\:saveButton').click(),
@@ -27,5 +34,7 @@ export const createInitialAppointment = async (page: Page, crn: string, eventNum
     createContact(page, crn, {
         relatesTo: `Event ${eventNumber} - ORA Community Order (6 Months)`,
         allocation: { team: team },
+        startTime: new Date(),
+        endTime: new Date(new Date().getTime() + 60000),
         ...data.contacts.initialAppointment,
     })
--- tests/person-search-index-from-delius/search-for-person.spec.ts
+++ tests/person-search-index-from-delius/search-for-person.spec.ts
@@ -4,6 +4,9 @@ import { createOffender } from '../../steps/delius/offender/create-offender.js'
 import { deliusPerson } from '../../steps/delius/utils/person.js'
 import * as dotenv from 'dotenv'
 import { doUntil } from '../../steps/delius/utils/refresh.js'
+import { createContact } from '../../steps/delius/contact/create-contact.js'
+import { LastMonth, Tomorrow, Yesterday } from '../../steps/delius/utils/date-time.js'
+
 dotenv.config() // read environment variables into process.env
 
 test('Create and search for a person', async ({ page }) => {
@@ -22,4 +25,54 @@ test('Create and search for a person', async ({ page }) => {
         () => page.keyboard.press('Enter'),
         () => expect(frame.locator('#live-offender-results')).toContainText(crn)
     )
+})
+
+test('Create and search for contacts', async ({ page }) => {
+    await deliusLogin(page)
+    const person = deliusPerson()
+    const crn = await createOffender(page, { person })
+    await createContact(page, crn, {
+        category: 'All/Always',
+        type: 'CV Created',
+        relatesTo: `Person - ${person.firstName} ${person.lastName}`,
+        date: LastMonth,
+    })
+    await createContact(page, crn, {
+        category: 'All/Always',
+        type: 'Other Contact',
+        relatesTo: `Person - ${person.firstName} ${person.lastName}`,
+        date: Yesterday,
+    })
+    await createContact(page, crn, {
+        category: 'Case Administration',
+        type: 'Clerical Officer Contact',
+        relatesTo: `Person - ${person.firstName} ${person.lastName}`,
+        date: new Date(),
+    })
+    await createContact(page, crn, {
+        category: 'Case Administration',
+        type: 'Clerical Officer Contact',
+        relatesTo: `Person - ${person.firstName} ${person.lastName}`,
+        date: Tomorrow,
+    })
+
+    await page.locator('input.btn', { hasText: /Text Search/ }).click()
+    await expect(page).toHaveTitle('Free Text Search')
+
+    await page.click('#textSearchButton')
+    // will have the four contacts above plus tier calculation
+    await doUntil(
+        () => page.click('#textSearchButton'),
+        () => expect(page.locator('tbody > tr')).toHaveCount(5)
+    )
+
+    await page.fill('#ContactFreeTextSearchForm\\:searchContents', 'CV')
+    await page.click('#textSearchButton')
+
+    await expect(page.locator('tbody > tr').first()).toContainText('CV Created')
+
+    await page.fill('#ContactFreeTextSearchForm\\:searchContents', 'Clerical Officer')
+    await page.click('#textSearchButton')
+
+    await expect(page.locator('tbody > tr')).toHaveCount(2)
 })
