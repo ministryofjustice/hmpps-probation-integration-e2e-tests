bbe0ad9be50712af10b556336e44bc4bf1cffe9c
smalepati99 <smalepati99@yahoo.com>
2023-03-16 18:52:12 +0000

    Fixed End2End tests, handle multi user login for OASys, Handle pages to complete the JS and added the recent changes from Approved Premises

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ const config: PlaywrightTestConfig = {
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 0 : 0,
+    retries: process.env.CI ? 2 : 0,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
