34fb9a018b090746df16cb14e3907d557f631726
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-04-20 14:36:05 +0100

    PI-1012 added ENV entry for EPF API

--- steps/api/epf/epf-api.ts
+++ steps/api/epf/epf-api.ts
@@ -1,5 +1,5 @@
-import { type APIRequestContext, expect, request } from '@playwright/test'
-import { getToken } from '../auth/get-token.js'
+import {type APIRequestContext, expect, request} from '@playwright/test'
+import {getToken} from '../auth/get-token.js'
 
 async function getContext(): Promise<APIRequestContext> {
     const token = await getToken()
@@ -12,9 +12,40 @@ async function getContext(): Promise<APIRequestContext> {
     })
 }
 
-export async function epfContext(crn: string, eventNumber: string): Promise<any> {
+export async function epfContext(crn: string, eventNumber: string): Promise<Response> {
     const response = await (await getContext()).get(`/case-details/${crn}/${eventNumber}`)
 
     expect(response.ok()).toBeTruthy()
     return response.json()
+}
+
+
+export interface Response {
+    name: Name;
+    dateOfBirth: Date;
+    gender: string;
+    sentence: Sentence;
+    responsibleProvider: ResponsibleProvider;
+    age: number;
+}
+
+export interface Name {
+    forename: string;
+    middleName: string;
+    surname: string;
+}
+
+export interface ResponsibleProvider {
+    code: string;
+    name: string;
+}
+
+export interface Sentence {
+    date: Date;
+    sentencingCourt: SentencingCourt;
+    releaseDate: Date;
+}
+
+export interface SentencingCourt {
+    name: string;
 }
