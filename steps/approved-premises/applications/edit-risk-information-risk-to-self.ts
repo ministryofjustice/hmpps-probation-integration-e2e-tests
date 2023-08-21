133209e67c0bb23a436c9a6f28ae2767c2774351
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-08-21 17:14:06 +0100

    Fix AP and Oasys

--- steps/approved-premises/applications/edit-risk-information-risk-to-self.ts
+++ steps/approved-premises/applications/edit-risk-information-risk-to-self.ts
@@ -6,7 +6,7 @@ export const verifyRiskToSelfIsAsPerOASys = async (page: Page) => {
         "OASys Question - 'Describe circumstances, relevant issues and needs regarding current concerns (refer to sections 1-12 for indicators, particularly Section 1' - Answer Input - 'Test concerns about self-harm and suicide'"
     )
     await expect(page.getByLabel('Current concerns about Coping in Custody or Hostel')).toContainText(
-        "OASys Question - 'R8.2 Coping in custody \\/ hostel setting - Describe circumstances, relevant issues and needs' - Answer Input - 'Test Issues and Needs'"
+        "OASys Question - 'R8.2 Coping in custody / hostel setting - Describe circumstances, relevant issues and needs' - Answer Input - 'Test Issues and Needs'"
     )
     await expect(page.getByLabel('Current concerns about Vulnerability')).toContainText(
         "OASys Question - 'R8.3.1 Are there any current concerns about vulnerability (eg victimisation, being bullied, assaulted, exploited) - Yes - Describe circumstances, relevant issues and needs' - Answer Input - 'Test Issues and Needs'"
