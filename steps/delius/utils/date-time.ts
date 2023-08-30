4b66e6e92d439df16f7be23815c0be77e6f609e5
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-08-30 16:09:39 +0100

    Updated R&M tests again (#329)

--- steps/delius/utils/date-time.ts
+++ steps/delius/utils/date-time.ts
@@ -1,4 +1,4 @@
-import { addMonths, addYears, addDays, subMonths, subDays, parse, format } from 'date-fns'
+import { addMonths, addYears, addDays, subMonths, subDays } from 'date-fns'
 export const options = { day: 'numeric', month: 'long', year: 'numeric' } as Intl.DateTimeFormatOptions
 export const recallDateFormatter = date => date.toLocaleDateString('en-GB', options)
 export const DeliusDateFormatter = (date: Date) => date.toLocaleDateString('en-GB')
@@ -8,10 +8,6 @@ export const EuropeLondonFormat = (date: Date) =>
     date.toLocaleString('sv', { timeZone: 'Europe/London' }).replace(' ', 'T')
 export const get12Hour = (date: Date) => (date.getHours() > 12 ? date.getHours() - 12 : date.getHours())
 export const getTimeOfDay = (date: Date) => (date.getHours() < 12 ? 'am' : 'pm')
-export const formatRMDateToDelius = (dateTime: string): string => {
-    const parsedDateTime = parse(dateTime, "h:mma 'on' d MMM yyyy", new Date())
-    return format(parsedDateTime, 'dd/MM/yyyy HH:mm')
-}
 
 //Getting dates using date-fns lib
 export const NextMonth = addMonths(new Date(), 1)
