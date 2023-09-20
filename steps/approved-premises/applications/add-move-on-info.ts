500b0e004a52b3c9febd6c3c341f64dbe4bc6ca0
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-09-20 10:07:45 +0100

    PI-1472 (#349)

--- steps/approved-premises/applications/add-move-on-info.ts
+++ steps/approved-premises/applications/add-move-on-info.ts
@@ -5,7 +5,7 @@ export const addMoveOnInformation = async (page: Page) => {
     await page.locator('#differentDuration-2').check()
     await page.locator('button', { hasText: 'Save and continue' }).click()
     await expect(page.locator('[for="postcodeArea"]')).toContainText(
-        /.*Where is [A-Z][a-z'’-]+([ -][A-Z][A-z'’-]+)* most likely to live when they move on from the AP.*/
+        /.*Where is the person most likely to live when they move on from the AP.*/
     )
     await page.locator('#postcodeArea').fill('SW11')
     await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
@@ -18,7 +18,7 @@ export const addMoveOnInformation = async (page: Page) => {
     await page.locator('#plansNotInPlaceDetail').fill(faker.lorem.paragraph())
     await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
     await expect(page.locator('#accommodationType-hint')).toContainText(
-        /.*What type of accommodation will [A-Z][a-z'’-]+([ -][A-Z][A-z'’-]+)* have when they leave the AP.*/
+        /.*What type of accommodation will the person have when they leave the AP.*/
     )
     await page.locator('#accommodationType').check()
     await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
