3cac94033efde337e9045eed8ff3aa427f4bcfd7
Marcus Aspin <maspin@unilink.com>
2023-10-03 16:53:11 +0100

    PI-1493 Add test for hmpps-auth-and-delius

---
+++ tests/hmpps-auth-and-delius/search-for-person.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@playwright/test'
+import * as dotenv from 'dotenv'
+
+dotenv.config() // read environment variables into process.env
+
+test('Sign in and check user details', async ({ page }) => {
+    await page.goto(`${process.env.AUTH_URL}/auth/sign-in`)
+    await expect(page).toHaveTitle(/HMPPS Digital Services - Sign in/)
+    await page.fill('#username', process.env.DELIUS_USERNAME!)
+    await page.fill('#password', process.env.DELIUS_PASSWORD!)
+    await page.click('#submit')
+    await expect(page).toHaveTitle(/HMPPS Digital Services - Home/)
+    await page.locator('[data-test="manage-account-link"]').click()
+    await expect(page.locator('[data-qa="email"]')).toContainText('automatedtestuser')
+})
