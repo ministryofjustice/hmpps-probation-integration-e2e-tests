dca31233430ee0bb50db33ab7f52154eac20d8c1
Marcus Aspin <maspin@unilink.com>
2023-10-25 08:52:28 +0100

    PI-1505 Added test for dps-and-delius (#374)
    * PI-1505 Added test for dps-and-delius
    
    * Release prisoner after test

--- steps/delius/document/create-document.ts
+++ steps/delius/document/create-document.ts
@@ -1,7 +1,7 @@
 import { expect, type Page } from '@playwright/test'
 import { selectOption } from '../utils/inputs'
 
-export async function createDocumentFromTemplate(page: Page, template: string) {
+export async function createDocumentFromTemplate(page: Page, template: string = null) {
     await page.locator('input', { hasText: 'Document' }).click()
     await expect(page).toHaveTitle(/Document List/)
     await selectOption(page, '#documentListForm\\:templateTypes', template)
