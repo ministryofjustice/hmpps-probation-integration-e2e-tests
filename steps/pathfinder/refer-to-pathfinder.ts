272ce439f960bb50d0d63b4b7bb766bd2b3a662e
Marcus Aspin <maspin@unilink.com>
2023-10-31 12:23:59 +0000

    PI-1081 End-to-end test for pathfinder-and-delius

---
+++ steps/pathfinder/refer-to-pathfinder.ts
@@ -0,0 +1,17 @@
+import { expect, type Page } from '@playwright/test'
+
+export const referToPathfinder = async (page: Page, crn: string) => {
+    // Search for CRN
+    await page.getByRole('link', { name: 'Refer from the community' }).click()
+    await page.getByLabel('CRN').fill(crn)
+    await page.getByRole('button', { name: 'Search' }).click()
+    await page.getByRole('link', { name: 'Refer to Pathfinder' }).click()
+    // Accept non-custodial case
+    await page.getByLabel('Yes').check()
+    await page.getByRole('button', { name: 'Save and continue' }).click()
+    // Enter reason for referral
+    await page.getByLabel('Reason for referral').fill('Automated testing')
+    await page.getByRole('button', { name: 'Save and continue' }).click()
+    // Check referral was successful
+    await expect(page.getByRole('heading', { name: 'Referral successful' })).toBeVisible()
+}
---
+++ tests/pathfinder-and-delius/refer-to-pathfinder.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+import { login as deliusLogin } from '../../steps/delius/login'
+import { login as pathfinderLogin } from '../../steps/pathfinder/login'
+import { createOffender } from '../../steps/delius/offender/create-offender'
+import { deliusPerson } from '../../steps/delius/utils/person'
+import { DeliusDateFormatter } from '../../steps/delius/utils/date-time'
+import { referToPathfinder } from '../../steps/pathfinder/refer-to-pathfinder'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+test('Refer a community case to pathfinder', async ({ page }) => {
+    // Given a new person in Delius
+    await deliusLogin(page)
+    const person = deliusPerson()
+    const crn = await createOffender(page, { person })
+
+    // When I refer them in pathfinder
+    await pathfinderLogin(page)
+    await referToPathfinder(page, crn)
+
+    // Then I can see their Delius details
+    await page.getByRole('button', { name: 'View summary' }).click()
+    await expect(page.locator('[data-qa=dob]')).toContainText(DeliusDateFormatter(person.dob))
+})
