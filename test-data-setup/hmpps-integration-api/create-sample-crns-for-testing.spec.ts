6398abec900215ba4300b06c42d028b71e9661a8
luke-a-williams <luke-a-williams@users.noreply.github.com>
2023-11-14 12:02:41 +0000

    Formatting changes

--- test-data-setup/hmpps-integration-api/create-sample-crns-for-testing.spec.ts
+++ test-data-setup/hmpps-integration-api/create-sample-crns-for-testing.spec.ts
@@ -2,14 +2,11 @@ import { test } from '@playwright/test'
 import { login as loginDelius } from '../../steps/delius/login'
 import { createOffender } from '../../steps/delius/offender/create-offender'
 import { createAndBookPrisoner } from '../../steps/api/dps/prison-api'
-import { deliusPerson } from "../../steps/delius/utils/person";
-
+import { deliusPerson } from '../../steps/delius/utils/person'
 
 test('create a crn for Delius and DPS but not OASyS', async ({ page }) => {
-
     await loginDelius(page)
-    const person = deliusPerson({sex: 'Male', dob: null, lastName: "Smith", firstName: "Bob"})
-    const crn: string = await createOffender(page, {person})
+    const person = deliusPerson({ sex: 'Male', dob: null, lastName: 'Smith', firstName: 'Bob' })
+    const crn: string = await createOffender(page, { person })
     await createAndBookPrisoner(page, crn, person)
-
 })
