15222f50ffde494455cd2b854903126c7f4c4614
Marcus Aspin <maspin@unilink.com>
2023-11-20 17:12:23 +0000

    Release prisoner after script run, to prevent reception area filling up

--- test-data-setup/hmpps-integration-api/create-sample-crn-for-testing.spec.ts
+++ test-data-setup/hmpps-integration-api/create-sample-crn-for-testing.spec.ts
@@ -1,13 +1,14 @@
 import { test } from '@playwright/test'
 import { login as loginDelius } from '../../steps/delius/login'
 import { createOffender } from '../../steps/delius/offender/create-offender'
-import { createAndBookPrisoner } from '../../steps/api/dps/prison-api'
+import { createAndBookPrisoner, releasePrisoner } from '../../steps/api/dps/prison-api'
 import { deliusPerson } from '../../steps/delius/utils/person'
 
 test('create a crn for Delius and DPS but not OASyS', async ({ page }) => {
     await loginDelius(page)
     const person = deliusPerson()
-    const crn: string = await createOffender(page, { person })
-    await createAndBookPrisoner(page, crn, person)
+    const crn = await createOffender(page, { person })
+    const { nomisId } = await createAndBookPrisoner(page, crn, person)
     console.log(crn, person)
+    await releasePrisoner(nomisId)
 })
