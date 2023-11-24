3fd3eeec8cb1626a4d46b41db768c2eb95f3fcf3
Raja Sekhar <raja.sekhar@digital.justice.gov.uk>
2023-11-24 13:25:28 +0000

    PI-1654 - added a function to create the PNC test data

--- steps/delius/utils/person.ts
+++ steps/delius/utils/person.ts
@@ -5,6 +5,7 @@ export interface Person {
     lastName: string
     sex: string
     dob: Date
+    pnc?: string
 }
 
 const sexOf = (str: string): SexType => {
@@ -21,10 +22,21 @@ export const deliusPerson = (person?: Person): Person => {
     const firstName = person?.firstName ? person.firstName : faker.person.firstName(sex)
     const lastName = person?.lastName ? person.lastName : faker.person.lastName(sex)
     const dob = person?.dob || faker.date.birthdate({ min: 18, max: 69, mode: 'age' })
+    const pnc = person?.pnc ? person.pnc : createPnc(dob)
     return {
         firstName,
         lastName,
         sex: sex[0].toUpperCase() + sex.substring(1),
         dob,
+        pnc,
     }
+}
+
+export function createPnc(date: Date): string {
+    const chars = 'ZABCDEFGHJKLMNPQRTUVWXY'
+    const year = date.getFullYear().toString()
+    const randomNumber = faker.number.int({ min: 0, max: 9999999 })
+    const padded = randomNumber.toString().padStart(7, '0')
+    const character = chars.charAt(+(year.substring(2) + padded) % 23)
+    return year + '/' + padded + character
 }
