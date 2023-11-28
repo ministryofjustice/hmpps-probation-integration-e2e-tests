649f180bbbdbb2c47752323bc24fb5dbb03b7143
Luke Williams <lawilliams1993@gmail.com>
2023-11-28 16:52:13 +0000

    Update hmpps integration api test to create offender with synthetic address

--- test-data-setup/hmpps-integration-api/create-sample-crn-for-testing.spec.ts
+++ test-data-setup/hmpps-integration-api/create-sample-crn-for-testing.spec.ts
@@ -3,11 +3,14 @@ import { login as loginDelius } from '../../steps/delius/login'
 import { createOffender } from '../../steps/delius/offender/create-offender'
 import { createAndBookPrisoner, releasePrisoner } from '../../steps/api/dps/prison-api'
 import { deliusPerson } from '../../steps/delius/utils/person'
+import {buildAddress, createAddress} from "../../steps/delius/address/create-address";
 
 test('create a crn for Delius and DPS but not OASyS', async ({ page }) => {
     await loginDelius(page)
     const person = deliusPerson()
     const crn = await createOffender(page, { person })
+    const address = buildAddress()
+    await createAddress(page, crn, address)
     const { nomisId } = await createAndBookPrisoner(page, crn, person)
     console.log(crn, person)
     await releasePrisoner(nomisId)
