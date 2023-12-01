f0a57675a467f5c3616d2218309cf71a9185ce14
Luke Williams <lawilliams1993@gmail.com>
2023-12-01 17:45:12 +0000

    remove changes to timeout used to test

--- steps/oasys/layer3-assessment/cms-search-results.ts
+++ steps/oasys/layer3-assessment/cms-search-results.ts
@@ -1,6 +1,6 @@
 import { type Page, expect } from '@playwright/test'
 
-export const clickCMSRecord = async (page: Page, timeout: number) => {
+export const clickCMSRecord = async (page: Page) => {
     await page.click('[headers="CMS_EVENT_NUMBER_R8794830689804117"]')
-    await expect(page.locator('#contextleft > h3')).toHaveText('CMS Offender Details', {timeout: timeout})
+    await expect(page.locator('#contextleft > h3')).toHaveText('CMS Offender Details')
 }
