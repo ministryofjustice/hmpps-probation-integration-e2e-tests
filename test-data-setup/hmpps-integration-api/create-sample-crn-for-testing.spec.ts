20040c7a329021c7f86929602cab3465e4129ff5
Luke Williams <lawilliams1993@gmail.com>
2023-12-01 17:31:27 +0000

    update new test to create offender without needs

--- test-data-setup/hmpps-integration-api/create-sample-crn-for-testing.spec.ts
+++ test-data-setup/hmpps-integration-api/create-sample-crn-for-testing.spec.ts
@@ -4,8 +4,13 @@ import { createOffender } from '../../steps/delius/offender/create-offender'
 import { createAndBookPrisoner, releasePrisoner } from '../../steps/api/dps/prison-api'
 import { deliusPerson } from '../../steps/delius/utils/person'
 import { buildAddress, createAddress } from '../../steps/delius/address/create-address'
+import {login as oasysLogin, UserType} from "../../steps/oasys/login";
+import {
+    createLayer3AssessmentWithoutNeeds
+} from "../../steps/oasys/layer3-assessment/create-layer3-assessment/create-layer3-without-needs";
+import {addLayer3AssessmentNeeds} from "../../steps/oasys/layer3-assessment/create-layer3-assessment/add-layer3-needs";
 
-test('create a crn for DPS and release them from Probation', async ({ page }) => {
+test('create a crn for DPS with an address and release them from Probation', async ({ page }) => {
     await loginDelius(page)
     const person = deliusPerson()
     const crn = await createOffender(page, { person })
@@ -13,5 +18,24 @@ test('create a crn for DPS and release them from Probation', async ({ page }) =>
     await createAddress(page, crn, address)
     const { nomisId } = await createAndBookPrisoner(page, crn, person)
     console.log(crn, person, nomisId)
+    await releasePrisoner(nomisId)
+})
+
+test('create a crn for DPS and Probation, with a layer 3 assessment, without needs', async ({ page }) => {
+    //Generate an offender
+    await loginDelius(page)
+    const person = deliusPerson()
+    const crn = await createOffender(page, { person })
+    const address = buildAddress()
+    const { nomisId } = await createAndBookPrisoner(page, crn, person)
+
+    //Print the ids into the test report log for ease of finding them.
+    console.log(crn, person, nomisId)
+
+    //Oasys - Risks, Scores and Needs data
+    await oasysLogin(page, UserType.Booking)
+    await createLayer3AssessmentWithoutNeeds(page, crn)
+
+    //Clean up the Probation Reception queue
     await releasePrisoner(nomisId)
 })
