94f79128369ad1b7e342ea95fecfb70556dbba5d
Raja Sekhar <raja.sekhar@digital.justice.gov.uk>
2023-12-11 19:21:52 +0000

    change the locator

--- steps/approved-premises/applications/reallocate.ts
+++ steps/approved-premises/applications/reallocate.ts
@@ -3,7 +3,7 @@ import { type Page } from '@playwright/test'
 export const reallocateApplication = async (page: Page, personName: string) => {
     await page.getByRole('link', { name: 'Workflow' }).click()
     await page.getByRole('link', { name: `Reallocate task for ${personName}` }).click()
-    await page.getByRole('combobox', { name: 'Reallocate task' }).selectOption({
+    await page.locator('#userId').selectOption({
         label: 'AutomatedTestUser AutomatedTestUser',
     })
     await page.getByRole('button', { name: 'Submit' }).click()
