bd26450ed5fdd57bc2fb38e53f725686d947930f
Luke Williams <108728588+luke-a-williams@users.noreply.github.com>
2023-12-12 11:53:44 +0000

    Hia 615 create synthetic offender with layer3 assessment (#423)
    * update common failing step to allow a larger timeout, to test
    
    * update new test to create offender without needs
    
    * remove changes to timeout used to test
    
    * add custodial event for risk assessment creation
    
    * update test to reflect the assessment state's incomplete steps
    
    * comment about the need for a manual completion of steps after the test has ran
    
    * remove obsolete address method
    
    * Formatting changes
    
    ---------
    
    Co-authored-by: probation-integration-bot <probation-integration-team@digital.justice.gov.uk>

--- test-data-setup/hmpps-integration-api/create-sample-crn-for-testing.spec.ts
+++ test-data-setup/hmpps-integration-api/create-sample-crn-for-testing.spec.ts
@@ -4,8 +4,12 @@ import { createOffender } from '../../steps/delius/offender/create-offender'
 import { createAndBookPrisoner, releasePrisoner } from '../../steps/api/dps/prison-api'
 import { deliusPerson } from '../../steps/delius/utils/person'
 import { buildAddress, createAddress } from '../../steps/delius/address/create-address'
+import { login as oasysLogin, UserType } from '../../steps/oasys/login'
+import { createLayer3AssessmentWithoutNeeds } from '../../steps/oasys/layer3-assessment/create-layer3-assessment/create-layer3-without-needs'
+import { addLayer3AssessmentNeeds } from '../../steps/oasys/layer3-assessment/create-layer3-assessment/add-layer3-needs'
+import { createCustodialEvent } from '../../steps/delius/event/create-event'
 
-test('create a crn for DPS and release them from Probation', async ({ page }) => {
+test('create a crn for DPS with an address and release them from Probation', async ({ page }) => {
     await loginDelius(page)
     const person = deliusPerson()
     const crn = await createOffender(page, { person })
@@ -14,4 +18,21 @@ test('create a crn for DPS and release them from Probation', async ({ page }) =>
     const { nomisId } = await createAndBookPrisoner(page, crn, person)
     console.log(crn, person, nomisId)
     await releasePrisoner(nomisId)
+})
+
+test('create a crn for Probation, with a layer 3 assessment in the incomplete state', async ({ page }) => {
+    //Generate an offender
+    await loginDelius(page)
+    const person = deliusPerson()
+    const crn = await createOffender(page, { person })
+
+    //Print the ids into the test report log for ease of finding them.
+    console.log(crn, person)
+
+    //Oasys - Risks, Scores and Needs data
+    await createCustodialEvent(page, { crn }) // required for OASys login to be able to create assessment
+    await oasysLogin(page, UserType.Booking)
+    await createLayer3AssessmentWithoutNeeds(page, crn)
+    await addLayer3AssessmentNeeds(page)
+    //Steps after this point to close an assessment must be manually completed.
 })
