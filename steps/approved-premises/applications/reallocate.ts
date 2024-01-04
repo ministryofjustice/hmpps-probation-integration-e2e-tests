a3e64e2f95ea99735234eb06aa7d8d2186bf0b48
Raja Sekhar <raja.sekhar@digital.justice.gov.uk>
2024-01-04 17:09:58 +0000

    remove pause

--- steps/approved-premises/applications/reallocate.ts
+++ steps/approved-premises/applications/reallocate.ts
@@ -3,7 +3,6 @@ import { type Page } from '@playwright/test'
 export const reallocateApplication = async (page: Page, personName: string) => {
     await page.getByRole('link', { name: 'Workflow' }).click()
     await page.getByRole('link', { name: `Reallocate task for ${personName}` }).click()
-    await page.pause()
     await page
         .getByRole('row', { name: /AutomatedTestUser AutomatedTestUser/ })
         .getByRole('button', { name: 'Allocate' })
