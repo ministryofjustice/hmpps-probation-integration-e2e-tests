5aab15410236355fd5987551e554d9370371a95b
Raja Sekhar <37211931+raja-sekhar1@users.noreply.github.com>
2024-01-05 09:33:31 +0000

    Fix the failing tests - Create a new case note and Create an approved premises application (#445)
    * fix the failing tests
    
    * get the allocate button text and click it.
    
    * remove pause

--- steps/approved-premises/applications/reallocate.ts
+++ steps/approved-premises/applications/reallocate.ts
@@ -3,8 +3,8 @@ import { type Page } from '@playwright/test'
 export const reallocateApplication = async (page: Page, personName: string) => {
     await page.getByRole('link', { name: 'Workflow' }).click()
     await page.getByRole('link', { name: `Reallocate task for ${personName}` }).click()
-    await page.getByRole('combobox', { name: 'Reallocate task' }).selectOption({
-        label: 'AutomatedTestUser AutomatedTestUser',
-    })
-    await page.getByRole('button', { name: 'Submit' }).click()
+    await page
+        .getByRole('row', { name: /AutomatedTestUser AutomatedTestUser/ })
+        .getByRole('button', { name: 'Allocate' })
+        .click()
 }
