ee058586513852a239d0049575fa6a7b11462c4d
Raja Sekhar <37211931+raja-sekhar1@users.noreply.github.com>
2024-02-06 13:55:36 +0000

    Fix test 'Create an approved premises application' (#483)
    * fix 'Create an approved premises application'
    
    * fix 'View OASys assessments in Approved Premises service'
    
    * lint error
    
    * added a new Oasys user OASYS_USERNAME_ASSESSMENT.
    update README.md
    
    * merge main and resolve conflicts
    
    * lint error
    
    * uptdate CI reference for the OaSys ASSESSMENT user
    
    * a small change
    
    * click the reallocate button differently
    
    * fix Create an approved premises application

--- steps/cas1-approved-premises/applications/confirm-details.ts
+++ steps/cas1-approved-premises/applications/confirm-details.ts
@@ -2,5 +2,7 @@ import { type Page, expect } from '@playwright/test'
 
 export const clickSaveAndContinue = async (page: Page) => {
     await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
+    await page.getByLabel('Very high risk').check()
+    await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
     await expect(page.locator('#main-content h1')).toHaveText('This application is not eligible')
 }
