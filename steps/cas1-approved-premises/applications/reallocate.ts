9f607cbba05a05daccbfbdb548b6d406343d49c8
Raja Sekhar <raja.sekhar@digital.justice.gov.uk>
2024-02-06 12:27:16 +0000

    fix Create an approved premises application

--- steps/cas1-approved-premises/applications/reallocate.ts
+++ steps/cas1-approved-premises/applications/reallocate.ts
@@ -2,6 +2,7 @@ import { type Page } from '@playwright/test'
 
 export const reallocateApplication = async (page: Page, personName: string) => {
     await page.getByRole('link', { name: 'Task allocation' }).click()
+    await page.getByRole('link', { name: 'Unallocated' }).click()
     await page.getByRole('link', { name: `${personName}` }).click()
     await page
         .getByRole('row', { name: /AutomatedTestUser AutomatedTestUser/ })
