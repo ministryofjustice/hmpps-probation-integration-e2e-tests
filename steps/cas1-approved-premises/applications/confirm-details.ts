a050c29282d3f5af47a4e7e78fdacf77a952873e
Raja Sekhar <raja.sekhar@digital.justice.gov.uk>
2024-02-07 16:19:40 +0000

    add a try catch for the test

--- steps/cas1-approved-premises/applications/confirm-details.ts
+++ steps/cas1-approved-premises/applications/confirm-details.ts
@@ -2,7 +2,10 @@ import { type Page, expect } from '@playwright/test'
 
 export const clickSaveAndContinue = async (page: Page) => {
     await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
-    await page.getByLabel('Very high risk').check()
-    await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
-    await expect(page.locator('#main-content h1')).toHaveText('This application is not eligible')
+    try {
+        await expect(page.locator('#main-content h1')).toHaveText('This application is not eligible')
+    } catch (error) {
+        await page.getByLabel('Very high risk').check()
+        await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
+    }
 }
