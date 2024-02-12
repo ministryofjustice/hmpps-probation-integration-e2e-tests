8ffe1ef5eacee227dc59ae19405e01a2d4372f71
Raja Sekhar <raja.sekhar@digital.justice.gov.uk>
2024-02-12 10:58:12 +0000

    update after comments

--- steps/tier-ui/search_tier.ts
+++ steps/tier-ui/search_tier.ts
@@ -3,7 +3,7 @@ import { Person } from '../delius/utils/person'
 
 export const searchTierByCRN = async (page: Page, crn: string, person: Person) => {
     await page.getByRole('button', { name: 'Start now' }).click()
-    await page.getByLabel('Search for a person on probation').fill(crn)
+    await page.getByLabel('Find a person on probation').fill(crn)
     await page.getByRole('button', { name: 'Search' }).click()
-    await page.getByRole('link', { name: `${person.firstName} ${person.lastName}` }).click()
+    await page.getByRole('link', { name: `${person.lastName}, ${person.firstName}` }).click()
 }
