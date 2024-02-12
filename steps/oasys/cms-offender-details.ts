addd4fa4db18f51a2e682b647f938b9a69f69de1
Raja Sekhar <37211931+raja-sekhar1@users.noreply.github.com>
2024-02-12 15:49:14 +0000

    Work around for slow delius to Oasys link. (#490)
    work around for slow delius to Oasys link.

--- steps/oasys/cms-offender-details.ts
+++ steps/oasys/cms-offender-details.ts
@@ -1,6 +1,7 @@
 import { type Page, expect } from '@playwright/test'
+import { refreshUntil } from '../delius/utils/refresh'
 
 export const clickCreateOffenderButton = async (page: Page) => {
     await page.click('#B8409510857074181')
-    await expect(page.locator('#contextleft > h3')).toHaveText('Offender Details')
+    await refreshUntil(page, () => expect(page.locator('#contextleft > h3')).toHaveText('Offender Details'))
 }
--- steps/oasys/layer3-assessment/cms-search-results.ts
+++ steps/oasys/layer3-assessment/cms-search-results.ts
@@ -1,6 +1,8 @@
 import { type Page, expect } from '@playwright/test'
+import { refreshUntil } from '../../delius/utils/refresh'
 
 export const clickCMSRecord = async (page: Page) => {
+    await refreshUntil(page, () => expect(page.locator("[headers='CMS_EVENT_NUMBER_R8794830689804117']")).toBeVisible())
     await page.click('[headers="CMS_EVENT_NUMBER_R8794830689804117"]')
     await expect(page.locator('#contextleft > h3')).toHaveText('Delius Offender Details')
 }
