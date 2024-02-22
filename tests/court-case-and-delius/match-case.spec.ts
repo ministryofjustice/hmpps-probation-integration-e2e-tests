8a6d446bc5c36e87f4c494d842c5319818a41361
Marcus Aspin <maspin@unilink.com>
2024-02-22 17:20:26 +0000

    PI-1906 Add function for adding a court hearing

---
+++ tests/court-case-and-delius/match-case.spec.ts
@@ -0,0 +1,15 @@
+import { test } from '@playwright/test'
+import { addCourtHearing } from '../../steps/court-case/add-court-hearing'
+import { deliusPerson } from '../../steps/delius/utils/person'
+
+test('Match Delius case with Court Case Hearing', async () => {
+    // Given a person with hearing in the Court Case Service
+    const person = deliusPerson()
+    await addCourtHearing(person)
+
+    // When I create the person's record in Delius
+    // ...
+
+    // Then the CRN is matched with the hearing and added to the court case service
+    // ...
+})
