66beb25db76534214e036c99016b6c9079ab9451
smalepati99 <smalepati99@yahoo.com>
2024-04-05 15:11:55 +0100

    Made changes to create-document & internal-transfer to address pipeline failures

--- steps/delius/transfer/internal-transfer.ts
+++ steps/delius/transfer/internal-transfer.ts
@@ -1,5 +1,5 @@
 import { expect, type Page } from '@playwright/test'
-import { selectOption } from '../utils/inputs'
+import {selectOption, selectOptionAndWait} from '../utils/inputs'
 import { findOffenderByCRN } from '../offender/find-offender'
 import { Allocation, Optional } from '../../../test-data/test-data'
 
@@ -18,13 +18,10 @@ export async function internalTransfer(
     await findOffenderByCRN(page, crn)
     await page.locator('input', { hasText: 'Transfers' }).click()
     await expect(page).toHaveTitle(/Consolidated Transfer Request/)
-    await selectOption(page, '#Trust\\:selectOneMenu', allocation?.team?.provider)
-    await selectOption(page, '#Team\\:selectOneMenu', allocation?.team?.name)
+    await selectOptionAndWait(page, '#Trust\\:selectOneMenu', allocation?.team?.provider)
+    await selectOptionAndWait(page, '#Team\\:selectOneMenu', allocation?.team?.name)
     await selectOption(page, '#Staff\\:selectOneMenu', allocation?.staff?.name)
 
-    await selectOption(page, '#Trust\\:selectOneMenu', allocation?.team?.provider)
-    await selectOption(page, '#Team\\:selectOneMenu', allocation?.team?.name)
-    await selectOption(page, '#Staff\\:selectOneMenu', allocation?.staff?.name)
     const options = await page
         .locator('#offenderTransferRequestTable')
         .locator('select')
