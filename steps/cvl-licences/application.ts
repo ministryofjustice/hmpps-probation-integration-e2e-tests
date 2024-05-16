e09618fa8a567e48108fbd9b08b5530d55f56191
smalepati99 <smalepati99@yahoo.com>
2024-05-16 17:11:57 +0100

    PI-2172: Implement End-to-End Test for 'View Case' in Create and Vary a Licence (CVL)

---
+++ steps/cvl-licences/application.ts
@@ -0,0 +1,49 @@
+import { expect, Page } from '@playwright/test'
+import { faker } from '@faker-js/faker'
+import {login as cvlLogin, loginAsPrisonOfficer} from "./login.js";
+
+export const createLicence = async (page: any, crn: string, nomsNumber: string) => {
+    await cvlLogin(page)
+    await page.getByRole('link', { name: 'Create and edit a licence before a release date' }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - Create a licence - Caseload')
+    await page.getByLabel('Find a case').fill(crn)
+    await page.getByRole('button', { name: 'Search' }).click()
+    await page.locator(`[href*="${nomsNumber}"]`).click();
+    await expect(page).toHaveTitle(/Create and vary a licence - Create a licence - Are you sure?/)
+    await page.getByRole('button', { name: /Continue and create licence/}).click()
+    await expect(page).toHaveTitle('Create and vary a licence - Create a licence - Who is the initial appointment with?')
+    await page.getByLabel(/Who is the initial appointment with?/).fill(faker.person.fullName())
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - Create a licence - Where is the initial appointment?')
+    await page.getByLabel(/Building and street/).first().fill(faker.location.buildingNumber() + " " + faker.location.street())
+    await page.getByLabel(/Town or city/).fill(faker.location.city())
+    await page.getByLabel('County (optional)').fill(faker.location.county());
+    await page.getByLabel(/Postcode/).fill(faker.location.zipCode())
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - Create a licence - What is the contact phone number for the initial appointment?')
+    await page.getByLabel('UK telephone number').fill('01632 960901');
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - Create a licence - When is the initial appointment?')
+    await page.getByLabel(/Immediately after release/).check()
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - Create a licence - Additional Post Sentence Supervision Conditions')
+    await page.getByLabel(/No/).check()
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - Create a licence - Check your answers')
+    await page.getByRole('button', { name: /Send to prison for approval/ }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - Create a licence - Confirmation')
+    await page.getByRole('link', { name: /Sign out/ }).click()
+};
+
+export const approveLicence = async (page: any, crn: string, nomsNumber: string) => {
+    await loginAsPrisonOfficer(page)
+    await page.getByRole('link', { name: 'Approve a licence' }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - approval cases')
+    await page.getByLabel('Find a case').fill(crn)
+    await page.locator(`tr:has([data-sort-value="${nomsNumber}"]) td#name-1 a.govuk-link`).click();
+    await expect(page).toHaveTitle('Create and vary a licence - Approve a licence')
+    await page.getByRole('button', { name: /Approve/ }).click()
+    await expect(page).toHaveTitle('Create and vary a licence - licence approved')
+    await page.locator('[data-qa$="user-name"]').click()
+    await page.getByRole('link', { name: /Sign out/ }).click()
+};
--- steps/cvl-licences/login.ts
+++ steps/cvl-licences/login.ts
@@ -8,3 +8,12 @@ export const login = async (page: Page) => {
     await page.click('#submit')
     await expect(page).toHaveTitle(/Create and vary a licence - Home/)
 }
+
+export const loginAsPrisonOfficer = async (page: Page) => {
+    await page.goto(process.env.CVL_URL)
+    await expect(page).toHaveTitle(/HMPPS Digital Services - Sign in/)
+    await page.fill('#username', process.env.DPS_USERNAME!)
+    await page.fill('#password', process.env.DPS_PASSWORD!)
+    await page.click('#submit')
+    await expect(page).toHaveTitle(/Create and vary a licence - Home/)
+}
