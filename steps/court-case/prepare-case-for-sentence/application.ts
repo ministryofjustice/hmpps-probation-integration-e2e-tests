87b10b668f582d765ddeb690b114a7dfb64e81c8
smalepati99 <smalepati99@yahoo.com>
2024-06-14 17:11:54 +0100

    PI-2259: Added test to verify the integration of court-case-and-delius apis

--- steps/court-case/prepare-case-for-sentence/application.ts
+++ steps/court-case/prepare-case-for-sentence/application.ts
@@ -1,4 +1,4 @@
-import { expect, Page } from '@playwright/test'
+import { expect, Locator, Page } from '@playwright/test'
 
 export async function addCourtToUser(page: Page, court: string) {
     await page.getByRole('button', { name: /Accept analytics cookies/ }).click()
@@ -12,16 +12,70 @@ export async function addCourtToUser(page: Page, court: string) {
     await expect(page).toHaveTitle('Case list - Prepare a case for sentence')
 }
 
-export async function getCRNByNameFromCaseList(page: Page, name: string): Promise<string | null> {
-    // Locate the row with the given name
-    const defendantRow = page.locator(`.govuk-table tbody tr:has-text("${name}")`)
+export async function searchAndClickDefendantAndGetHeader(
+    page: Page,
+    firstName: string,
+    lastName: string,
+    crn: string
+): Promise<Locator> {
+    await page.locator('#search-term').fill(crn)
+    await page.getByRole('button', { name: 'Search' }).click()
+    await page.locator('.pac-defendant-link', { hasText: `${firstName} ${lastName}` }).click()
 
-    // Extract the CRN from the row
-    const crnElement = defendantRow.locator('.govuk-table__cell .pac-secondary-text')
+    // Return the locator for '.pac-key-details-bar'
+    return page.locator('.pac-key-details-bar')
+}
 
-    // Get the inner text of the CRN element
-    const crnText = await crnElement.innerText()
+export async function formatDateToPrepareCase(dateString: string): Promise<string> {
+    // Split the date string into day, month, and year
+    const parts = dateString.split('/')
+    if (parts.length !== 3) {
+        throw new Error('Invalid date format. Expected DD/MM/YYYY.')
+    }
 
-    // Return the CRN
-    return crnText.trim() !== '' ? crnText.trim() : null
+    const day = parseInt(parts[0], 10)
+    const month = parseInt(parts[1], 10) - 1 // Months are zero-indexed in JavaScript Date
+    const year = parseInt(parts[2], 10)
+
+    // Construct a new Date object
+    const dateObject = new Date(year, month, day)
+
+    // Format the date as required
+    const formattedDate = dateObject.toLocaleDateString('en-GB', {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+    })
+
+    return formattedDate
+}
+
+export async function extractRegistrationDetails(page: Page) {
+    const [registrationType, registeredDate, nextReviewDate] = await page.evaluate(() => {
+        const activeTab = document.querySelector('.govuk-tabs__panel#active')
+        const table = activeTab?.querySelector('.govuk-table')
+        if (!table) {
+            return ['', '', '']
+        }
+        const firstRow = table.querySelector('.govuk-table__body .govuk-table__row')
+        if (!firstRow) {
+            return ['', '', '']
+        }
+        const cells = firstRow.querySelectorAll('.govuk-table__cell')
+        return [
+            cells?.[0]?.textContent?.trim() ?? '',
+            cells?.[1]?.textContent?.trim() ?? '',
+            cells?.[2]?.textContent?.trim() ?? '',
+        ]
+    })
+
+    return { registrationType, registeredDate, nextReviewDate }
+}
+
+export async function extractProbationRecordDetails(page: Page): Promise<{ outcome: string; offence: string }> {
+    await page.getByRole('link', { name: 'Probation record' }).click()
+    await expect(page).toHaveTitle('Probation record - Prepare a case for sentence')
+    const outcome = await page.locator('[href^="record/"]').innerText()
+    const offence = await page.locator('p:nth-of-type(2)').first().innerText()
+    return { outcome, offence }
 }
