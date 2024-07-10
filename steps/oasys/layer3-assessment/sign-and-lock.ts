c9d3bc6520ce3ba2f05575a6d815d106a7e6d5f9
smalepati99 <42004985+smalepati99@users.noreply.github.com>
2024-07-10 09:30:52 +0100

    PI-2347: Updated OASys function for sign and lock to address the OASys failing flaky tests (#633)
    * PI-2347: Updated OASys function for sign and lock
    
    * PI-2347: Updated OASys function for sign and lock to address the OASys failing flaky tests

--- steps/oasys/layer3-assessment/sign-and-lock.ts
+++ steps/oasys/layer3-assessment/sign-and-lock.ts
@@ -6,12 +6,10 @@ export const signAndlock = async (page: Page, role?: string) => {
     await page.getByRole('button', { name: 'Sign & Lock' }).click()
     await page.getByRole('button', { name: 'Mark 1 to 9 as Missing' }).click()
 
-    // Click 'Continue with Signing' if role is not 'ApprovedPSORole'
-    if (role !== 'ApprovedPSORole') {
-        const continueButton = await page.locator('[value="Continue with Signing"]')
-        if (await continueButton.isVisible()) {
-            await continueButton.click()
-        }
+    // Click on 'Continue with Signing' if it is visible
+    const continueButton = await page.locator('[value="Continue with Signing"]')
+    if (await continueButton.isVisible()) {
+        await continueButton.click()
     }
 
     // Click 'Confirm Sign & Lock'
