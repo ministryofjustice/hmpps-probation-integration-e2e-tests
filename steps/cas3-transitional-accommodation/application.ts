127bd7e5dcd254c3ff5bac2b44ada4ca243954ad
smalepati99 <smalepati99@yahoo.com>
2024-07-26 18:14:27 +0100

    Incorporated latest application changes for Delius SAR & CAS3 Tests

--- steps/cas3-transitional-accommodation/application.ts
+++ steps/cas3-transitional-accommodation/application.ts
@@ -123,7 +123,6 @@ async function enterContactDetails(page: Page) {
     await page.getByLabel('Phone number').fill(faker.phone.number())
     await page.getByLabel('Email').fill(faker.internet.email())
     await page.getByRole('button', { name: 'Save and continue' }).click()
-
     await expect(page).toHaveTitle("What is the person's phone number? - Transitional Accommodation (CAS3)")
     await page
         .getByLabel('Please provide an up to date contact number for the person on probation.')
