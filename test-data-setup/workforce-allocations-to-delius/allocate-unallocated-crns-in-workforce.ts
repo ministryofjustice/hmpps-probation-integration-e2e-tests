0c3310904495c40c979e8bb0df2f57d53c9a4b7d
Marcus Aspin <maspin@unilink.com>
2024-08-23 06:36:24 +0000

    PI-2455 Add weekly CI job to allocate cases

--- test-data-setup/workforce-allocations-to-delius/allocate-unallocated-crns-in-workforce.ts
+++ test-data-setup/workforce-allocations-to-delius/allocate-unallocated-crns-in-workforce.ts
@@ -1,17 +1,10 @@
 import { login } from '../../steps/delius/login'
 import { login as workforceLogin } from '../../steps/workforce/login'
 import { internalTransfer } from '../../steps/delius/transfer/internal-transfer'
-import { Allocation, data } from '../../test-data/test-data'
+import { data } from '../../test-data/test-data'
 import { chromium, expect, Page, test } from '@playwright/test'
 import { slow } from '../../steps/common/common'
 import { refreshUntil } from '../../steps/delius/utils/refresh.js'
-
-test.beforeEach(async ({ page }) => {
-    await login(page)
-})
-
-// another staff member in the same team, for currently/previously allocated cases:
-const anotherPractitioner: Allocation = { staff: data.staff.allocationsTester2, team: data.teams.allocationsTestTeam }
 
 test('Allocate unallocated workforce crns in delius', async ({ page }) => {
     slow()
@@ -34,7 +27,10 @@ export const allocateCases = async (page: Page, number: number) => {
         console.log('crn from workforce is ' + crn)
 
         try {
-            await internalTransfer(deliusPage, { crn, allocation: anotherPractitioner })
+            await internalTransfer(deliusPage, {
+                crn,
+                allocation: { staff: data.staff.allocationsTester2, team: data.teams.allocationsTestTeam },
+            })
         } catch (error) {
             console.error(`Error occurred during internal transfer: ${error}`)
         }
