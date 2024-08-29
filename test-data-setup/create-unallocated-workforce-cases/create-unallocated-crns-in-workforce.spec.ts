cbfcaa113e613634ae777c500b7f11b23b7a32b2
smalepati99 <42004985+smalepati99@users.noreply.github.com>
2024-08-29 12:33:41 +0100

    PI-2484 - Add Script and GitHub Action for Creating Unallocated Cases; Refactor allocateCases to Allocate Up to 100 Cases Based on Unallocated Count (#679)
    * PI-2484 - Add Script and GitHub Action for Creating Unallocated Cases; Refactor allocateCases to Allocate Up to 100 Cases Based on Unallocated Count
    
    * PI-2484 - Add Script and GitHub Action for Creating Unallocated Cases; Refactor allocateCases to Allocate Up to 100 Cases Based on Unallocated Count
    
    * PI-2484 - Add Script and GitHub Action for Creating Unallocated Cases; Refactor allocateCases to Allocate Up to 100 Cases Based on Unallocated Count
    
    * PI-2484 - Add Script and GitHub Action for Creating Unallocated Cases; Refactor allocateCases to Allocate Up to 100 Cases Based on Unallocated Count
    
    * PI-2484 - Add Script and GitHub Action for Creating Unallocated Cases; Refactor allocateCases to Allocate Up to 100 Cases Based on Unallocated Count
    
    * PI-2484 - Changed the file names to match with the tests

---
+++ test-data-setup/create-unallocated-workforce-cases/create-unallocated-crns-in-workforce.spec.ts
@@ -0,0 +1,36 @@
+import { login as deliusLogin } from '../../steps/delius/login'
+import { data } from '../../test-data/test-data'
+import { Page, test } from '@playwright/test'
+import { slow } from '../../steps/common/common'
+import { createOffender } from '../../steps/delius/offender/create-offender.js'
+import { createCommunityEvent } from '../../steps/delius/event/create-event.js'
+import { createRequirementForEvent } from '../../steps/delius/requirement/create-requirement.js'
+import { createInitialAppointment } from '../../steps/delius/contact/create-contact.js'
+
+test.beforeEach(async ({ page }) => {
+    await deliusLogin(page)
+})
+
+test('Create cases awaiting Allocation', async ({ page }) => {
+    slow()
+    await createCasesAwaitingAllocation(page, 50)
+})
+
+const createCasesAwaitingAllocation = async (page: Page, number: number) => {
+    await deliusLogin(page)
+
+    for (let i = 0; i < number; i++) {
+        const crn = await createOffender(page, { providerName: data.teams.allocationsTestTeam.provider })
+
+        await createCommunityEvent(page, {
+            crn,
+            allocation: {
+                team: data.teams.allocationsTestTeam,
+                staff: data.staff.unallocated,
+            },
+        })
+
+        await createRequirementForEvent(page, { crn, team: data.teams.allocationsTestTeam })
+        await createInitialAppointment(page, crn, '1', data.teams.allocationsTestTeam)
+    }
+}
--- test-data-setup/workforce-allocations-to-delius/allocate-unallocated-crns-in-workforce.spec.ts
+++ test-data-setup/workforce-allocations-to-delius/allocate-unallocated-crns-in-workforce.spec.ts
@@ -1,4 +1,4 @@
-import { login } from '../../steps/delius/login'
+import { login as deliusLogin } from '../../steps/delius/login'
 import { login as workforceLogin } from '../../steps/workforce/login'
 import { internalTransfer } from '../../steps/delius/transfer/internal-transfer'
 import { data } from '../../test-data/test-data'
@@ -6,33 +6,46 @@ import { chromium, expect, Page, test } from '@playwright/test'
 import { slow } from '../../steps/common/common'
 import { refreshUntil } from '../../steps/delius/utils/refresh.js'
 
-test('Allocate unallocated workforce crns in delius', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
+    await deliusLogin(page)
+})
+
+test('Allocate unallocated workforce CRNs in Delius', async ({ page }) => {
     slow()
     await allocateCases(page, 100)
 })
 
-export const allocateCases = async (page: Page, number: number) => {
+const allocateCases = async (page: Page, number: number) => {
     await workforceLogin(page)
-    await refreshUntil(page, () => expect(page.locator("[href$='find-unallocated']")).toBeVisible())
+
+    // Navigate to the unallocated cases page
     await page.getByRole('button', { name: /View unallocated cases/ }).click()
     await refreshUntil(page, () => expect(page).toHaveTitle(/.*Unallocated cases.*/))
-    await expect(page).toHaveTitle(/.*Unallocated cases.*/)
-    await page.locator('[data-persistent-id="find-unallocated-cases-sentence-date"]').click()
-    const browser = await chromium.launch()
-    const deliusPage = await browser.newPage()
-    await login(deliusPage)
 
-    for (let i = 1; i <= number; i++) {
-        const crn = await page.locator(`#main-content table tr:nth-child(${i}) td:first-child span`).textContent()
-        console.log('crn from workforce is ' + crn)
+    // Retrieve and parse the number of unallocated cases
+    const unallocatedText = await page.getByText(/Unallocated cases \(\d+\)/).textContent()
+    const unallocatedCount = parseInt(unallocatedText.match(/\d+/)[0], 10)
 
-        try {
-            await internalTransfer(deliusPage, {
-                crn,
-                allocation: { staff: data.staff.allocationsTester2, team: data.teams.allocationsTestTeam },
-            })
-        } catch (error) {
-            console.error(`Error occurred during internal transfer: ${error}`)
+    if (unallocatedCount > number + 50) {
+        const browser = await chromium.launch()
+        const deliusPage = await browser.newPage()
+        await deliusLogin(deliusPage)
+
+        for (let i = 1; i <= number; i++) {
+            const crn = await page.locator(`#main-content table tr:nth-child(${i}) td:first-child span`).textContent()
+            console.log('CRN from workforce is ' + crn)
+
+            try {
+                await internalTransfer(deliusPage, {
+                    crn,
+                    allocation: { staff: data.staff.allocationsTester2, team: data.teams.allocationsTestTeam },
+                })
+            } catch (error) {
+                console.error(`Error occurred during internal transfer: ${error}`)
+            }
         }
+        await browser.close()
+    } else {
+        console.log(`Number of unallocated cases is ${unallocatedCount}. Allocation will not proceed.`)
     }
 }
