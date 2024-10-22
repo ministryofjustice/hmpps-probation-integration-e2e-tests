334b7b9439a14d23b85ebfb7ec360b84c110f28e
smalepati99 <srikanth.malepati1@digital.justice.gov.uk>
2024-10-22 16:07:21 +0100

    PI-2602 - Fix failing probation integration end-to-end tests in the pipeline

--- steps/cas1-approved-premises/applications/location-factors.ts
+++ steps/cas1-approved-premises/applications/location-factors.ts
@@ -7,7 +7,20 @@ export const addLocationFactors = async (page: Page) => {
     await page.locator('#alternativeRadiusAccepted-2').check()
     await page.locator('#restrictions-2').check()
     await page.locator('.govuk-button', { hasText: 'Save and continue' }).click()
-    await expect(page).toHaveTitle('Approved Premises - Select a preferred AP')
-    await selectOption(page, '#preferredAp1', 'Test AP 1')
+
+    const title = await page.title()
+
+    if (title.includes("Select all preferred properties for your women’s AP application")) {
+        // If the title is for the female case
+        await expect(page).toHaveTitle(/Approved Premises - Select all preferred properties for your women’s AP application/)
+        await selectOption(page, '#preferredAp1', 'Test Female AP 1')
+    } else if (title === 'Approved Premises - Select a preferred AP') {
+        // If the title is for the male case
+        await expect(page).toHaveTitle(/Approved Premises - Select a preferred AP/)
+        await selectOption(page, '#preferredAp1', 'Test AP 1')
+    } else {
+        throw new Error(`Unexpected page title: ${title}`)
+    }
+
     await page.click('button.govuk-button')
 }
--- steps/cas1-approved-premises/applications/reallocate.ts
+++ steps/cas1-approved-premises/applications/reallocate.ts
@@ -1,9 +1,13 @@
 import { type Page } from '@playwright/test'
 
-export const reallocateApplication = async (page: Page, personName: string) => {
+export const reallocateApplication = async (page: Page, person: { firstName: string; lastName: string; sex: string }) => {
     await page.getByRole('link', { name: 'Task allocation' }).click()
     await page.getByRole('link', { name: 'Unallocated' }).click()
-    await page.getByRole('link', { name: `${personName}` }).click()
+    if (person.sex === 'Female') {
+        await page.getByLabel('AP area').selectOption('all')
+        await page.getByRole('button', { name: 'Apply filters' }).click()
+    }
+    await page.getByRole('link', { name: `${person.firstName} ${person.lastName}` }).click()
     await page
         .getByRole('row', { name: /AutomatedTestUser AutomatedTestUser/ })
         .getByRole('button', { name: 'Allocate' })
