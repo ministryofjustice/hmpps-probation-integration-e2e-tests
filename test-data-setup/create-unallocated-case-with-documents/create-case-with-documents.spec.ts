ac9a5386e403b2a686612e5df1575ff9084f8f2f
Iain Chambers <iain.chambers@digital.justice.gov.uk>
2024-12-11 09:02:42 +0000

    Move document creation block

--- test-data-setup/create-unallocated-case-with-documents/create-case-with-documents.spec.ts
+++ test-data-setup/create-unallocated-case-with-documents/create-case-with-documents.spec.ts
@@ -27,6 +27,10 @@ const createCaseWithDocuments = async (page: Page, number: number) => {
         },
     })
 
+    for (let i = 0; i < number; i++) {
+        await createDocumentFromTemplate(page)
+    }
+
     await createCommunityEvent(page, {
         crn,
         allocation: {
@@ -38,7 +42,5 @@ const createCaseWithDocuments = async (page: Page, number: number) => {
     await createRequirementForEvent(page, { crn, team: data.teams.allocationsTestTeam })
     await createInitialAppointment(page, crn, '1', data.teams.allocationsTestTeam)
 
-    for (let i = 0; i < number; i++) {
-        await createDocumentFromTemplate(page)
-    }
+    
 }
