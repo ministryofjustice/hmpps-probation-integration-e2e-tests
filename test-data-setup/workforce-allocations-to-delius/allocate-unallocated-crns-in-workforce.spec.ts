f13e81caba444e539ce244737e7ba81daba1219f
Marcus Aspin <maspin@unilink.com>
2025-05-19 13:20:39 +0100

    PI-3093 Fix workforce allocations cleanup script

--- test-data-setup/workforce-allocations-to-delius/allocate-unallocated-crns-in-workforce.spec.ts
+++ test-data-setup/workforce-allocations-to-delius/allocate-unallocated-crns-in-workforce.spec.ts
@@ -6,32 +6,30 @@ import { chromium, expect, Page, test } from '@playwright/test'
 import { slow } from '../../steps/common/common'
 import { refreshUntil } from '../../steps/delius/utils/refresh.js'
 
-test.beforeEach(async ({ page }) => {
-    await deliusLogin(page)
-})
-
 test('Allocate unallocated workforce CRNs in Delius', async ({ page }) => {
     slow()
-    await allocateCases(page, 100)
-})
 
-const allocateCases = async (page: Page, number: number) => {
-    await workforceLogin(page)
+    const minimumToLeaveUnallocated = 50
+    const maximumToAllocate = 50
 
     // Navigate to the unallocated cases page
+    await workforceLogin(page)
     await page.getByRole('button', { name: /View unallocated cases/ }).click()
     await refreshUntil(page, () => expect(page).toHaveTitle(/.*Unallocated cases.*/))
 
     // Retrieve and parse the number of unallocated cases
-    const unallocatedText = await page.getByText(/Unallocated cases \(\d+\)/).textContent()
+    const unallocatedText = await page.getByText(/Unallocated cases \(\d+\+?\)/).textContent()
     const unallocatedCount = parseInt(unallocatedText.match(/\d+/)[0], 10)
 
-    if (unallocatedCount > number + 50) {
+    if (unallocatedCount > minimumToLeaveUnallocated) {
+        const casesToAllocate = Math.min(unallocatedCount - minimumToLeaveUnallocated, maximumToAllocate)
+        console.log(`Allocating ${casesToAllocate} of ${unallocatedCount} cases`)
+
         const browser = await chromium.launch()
         const deliusPage = await browser.newPage()
         await deliusLogin(deliusPage)
 
-        for (let i = 1; i <= number; i++) {
+        for (let i = 1; i <= Math.min(unallocatedCount - minimumToLeaveUnallocated, maximumToAllocate); i++) {
             const crn = await page.locator(`#main-content table tr:nth-child(${i}) td:first-child span`).textContent()
             console.log('CRN from workforce is ' + crn)
 
@@ -48,4 +46,4 @@ const allocateCases = async (page: Page, number: number) => {
     } else {
         console.log(`Number of unallocated cases is ${unallocatedCount}. Allocation will not proceed.`)
     }
-}
+})
