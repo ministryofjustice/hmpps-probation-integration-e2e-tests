dbf4179ffd2d707e388c5e962517724f5fafa30d
Marcus Aspin <maspin@unilink.com>
2025-09-18 13:42:24 +0100

    Fix exports (#861)
    so dependents don't need to import with a .mjs file extension

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
     entry: ['test-data/**', 'steps/**'],
+    exports: {
+        all: true,
+        customExports(exports) {
+            for (const e of Object.keys(exports)) exports[e] = exports[e].replace(/^\.\/dist\//, './')
+            return exports
+        },
+    },
     unbundle: true,
     outDir: 'dist',
     format: 'esm',
