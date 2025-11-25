3d6e9b0e7d65bb9d231bc731b3e8ca00d25d129c
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-11-25 14:21:31 +0000

    PI-3629 add message group id (#900)
    * PI-3629 add message group id
    
    ---------
    
    Co-authored-by: Marcus Aspin <maspin@unilink.com>

--- tests/common-platform-and-delius/search-for-person.spec.ts
+++ tests/common-platform-and-delius/search-for-person.spec.ts
@@ -5,6 +5,7 @@ import { runPod } from '../../steps/k8s/k8s-utils'
 import { DateTime } from 'luxon'
 import { hearingData } from '../../steps/court-case/hearing-data'
 import { buildAddress } from '../../steps/delius/address/create-address'
+import { randomUUID } from 'crypto'
 
 test('Create and search for a person', async ({ page }) => {
     const person = deliusPerson()
@@ -13,13 +14,17 @@ test('Create and search for a person', async ({ page }) => {
         'court-probation-dev',
         'probation-integration-e2e-tests',
         'court-facing-api',
-        ['aws sns publish --topic-arn "$TOPIC_ARN" --message "$MESSAGE" --message-attributes "$ATTRIBUTES"'],
+        [
+            'aws sns publish --topic-arn "$TOPIC_ARN" --message "$MESSAGE" --message-attributes "$ATTRIBUTES" --message-group-id "$MESSAGE_GROUP_ID"',
+        ],
         [
             { name: 'TOPIC_ARN', valueFrom: { secretKeyRef: { name: 'court-cases-topic', key: 'topic_arn' } } },
             { name: 'MESSAGE', value: JSON.stringify(hearingData(person, address)) },
+            { name: 'MESSAGE_GROUP_ID', value: randomUUID() },
             {
                 name: 'ATTRIBUTES',
                 value: JSON.stringify({
+                    eventType: { DataType: 'String', StringValue: 'commonplatform.case.received' },
                     messageType: { DataType: 'String', StringValue: 'COMMON_PLATFORM_HEARING' },
                     hearingEventType: { DataType: 'String', StringValue: 'ConfirmedOrUpdated' },
                 }),
