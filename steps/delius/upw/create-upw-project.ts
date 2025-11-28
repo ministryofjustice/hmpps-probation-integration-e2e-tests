8d299244bf6dd2c3c587f4e6c402bf07d8165629
Dhruv Patel <dhruv.patel@justice.gov.uk>
2025-11-28 15:54:16 +0000

    Update steps/delius/upw/create-upw-project.ts
    Co-authored-by: Marcus Aspin <maspin@unilink.com>

--- steps/delius/upw/create-upw-project.ts
+++ steps/delius/upw/create-upw-project.ts
@@ -57,15 +57,7 @@ export async function createUpwProject(
 }
 
 export function createNameWithTimeStamp(prefix = 'project'): string {
-    const now = new Date()
-    const dateFormat =
-        now.getDate().toString().padStart(2, '0') +
-        (now.getMonth() + 1).toString().padStart(2, '0') +
-        now.getFullYear() +
-        now.getHours().toString().padStart(2, '0') +
-        now.getMinutes().toString().padStart(2, '0') +
-        now.getSeconds().toString().padStart(2, '0')
-    return `${prefix}-${dateFormat}`
+    return `${prefix}-${DateTime.now().toFormat('ddMMyyyyHHmmss')}`
 }
 
 async function addProjectAvailability(
