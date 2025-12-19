fb4ab477c8233a385c551ac2eccd9b036a5bb9b7
smalepati99 <42004985+smalepati99@users.noreply.github.com>
2025-12-19 14:32:15 +0000

    PI-3717 - Fix failing E2E tests after recent app updates (#913)

--- steps/esupervision/check-in.ts
+++ steps/esupervision/check-in.ts
@@ -54,7 +54,7 @@ export async function createCheckin(page: Page, uuid: string, person: Person) {
 
     await page.getByRole('button', { name: 'Continue' }).click()
     await page.getByRole('button', { name: 'Start recording' }).click()
-    await page.getByRole('button', { name: 'Continue' }).click()
+    await page.getByRole('button', { name: /Submit video anyway/ }).click()
 
     await page.getByRole('checkbox', { name: /I confirm/ }).check()
     await page.getByRole('button', { name: 'Complete check in' }).click()
