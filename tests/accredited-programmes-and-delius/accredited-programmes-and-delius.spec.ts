561aea4b2bed9fa156ca9257c29b75d7c5b14a30
moyow-moj <wendy.moyo@justice.gov.uk>
2026-05-14 14:41:50 +0100

    Removing unused import

--- tests/accredited-programmes-and-delius/accredited-programmes-and-delius.spec.ts
+++ tests/accredited-programmes-and-delius/accredited-programmes-and-delius.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test'
+import { test } from '@playwright/test'
 import { login as deliusLogin } from '../../steps/delius/login'
 import { createOffender } from '../../steps/delius/offender/create-offender'
 import { deliusPerson } from '../../steps/delius/utils/person'
