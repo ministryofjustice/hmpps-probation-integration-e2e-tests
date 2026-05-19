06606cfab60fac268a82de8f1001c2af38b315d3
Wendy Moyo <wendy.moyo@justice.gov.uk>
2026-05-19 11:19:29 +0100

    Test fixes and code refactoring (#1022)
    * Test fixes and refactor
    
    * Updates

---
+++ steps/community-payback/login.ts
@@ -0,0 +1,19 @@
+import { expect, type Page } from '@playwright/test'
+
+export const loginAsCaseAdmin = async (page: Page) => {
+    await page.goto(process.env.COMMUNITY_PAYBACK_URL)
+    await expect(page).toHaveTitle(/HMPPS Digital Services - Sign in/)
+    await page.fill('#username', process.env.DELIUS_USERNAME!)
+    await page.fill('#password', process.env.DELIUS_PASSWORD!)
+    await page.click('#submit')
+    await expect(page).toHaveTitle(/Community Payback/)
+}
+
+export const loginAsSupervisor = async (page: Page) => {
+    await page.goto(process.env.COMMUNITY_PAYBACK_SUPERVISORS_URL)
+    await expect(page).toHaveTitle(/HMPPS Digital Services - Sign in/)
+    await page.fill('#username', process.env.DELIUS_USERNAME!)
+    await page.fill('#password', process.env.DELIUS_PASSWORD!)
+    await page.click('#submit')
+    await expect(page.locator('h1.govuk-heading-l')).toContainText(/Next sessions/)
+}
