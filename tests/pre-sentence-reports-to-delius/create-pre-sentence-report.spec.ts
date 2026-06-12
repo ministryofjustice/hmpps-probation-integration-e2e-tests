ab302e5a4643db8556fc10d787e457e15b2b0bbb
moyow-moj <wendy.moyo@justice.gov.uk>
2026-06-12 10:57:01 +0100

    test updates

--- tests/pre-sentence-reports-to-delius/create-pre-sentence-report.spec.ts
+++ tests/pre-sentence-reports-to-delius/create-pre-sentence-report.spec.ts
@@ -13,6 +13,7 @@ import { buildAddress, createAddress } from '../../steps/delius/address/create-a
 import { selectOption } from '../../steps/delius/utils/inputs'
 import fs from 'fs'
 import { refreshUntil } from '../../steps/delius/utils/refresh'
+import { Yesterday } from '../../steps/delius/utils/date-time'
 
 test('Create a pre-sentence report', async ({ page }) => {
     // Given a person with an event that has been adjourned for a pre-sentence report,
@@ -23,10 +24,12 @@ test('Create a pre-sentence report', async ({ page }) => {
     const crn = await createOffender(page, { person })
     const address = buildAddress()
     await createAddress(page, crn, address)
+    const offenceDate = Yesterday.toJSDate()
     const event = await createEvent(page, {
         crn,
         event: data.events.adjournedForFastPreSentenceReport,
         allocation: { team: data.teams.genericTeam, staff: data.staff.genericStaff },
+        date: offenceDate,
     })
     await page.locator('input', { hasText: 'Save' }).click()
     await findCourtReport(page, crn)
@@ -60,14 +63,18 @@ test('Create a pre-sentence report', async ({ page }) => {
     await expect(popup.locator('//tbody/tr[@class="govuk-table__row"]/td[1]')).toContainText(
         data.events.adjournedForFastPreSentenceReport.mainOffence.split(' -')[0]
     )
-    await popup.locator('#offencesUnderConsideration').click()
-    await popup.keyboard.type(faker.lorem.sentence())
-    await popup.locator('#offencesPattern').fill(faker.lorem.sentence())
+    await expect(popup.locator('//tbody/tr[@class="govuk-table__row"]/td[2]')).toContainText(
+        offenceDate.toLocaleDateString('en-GB')
+    )
+    for (const textarea of await popup.locator('[contenteditable]').elementHandles()) {
+        await textarea.click()
+        await popup.keyboard.type(faker.lorem.sentence())
+    }
     await popup.locator('text=Save and continue').click()
 
     // - Defendant behaviour and lifestyle assessment
     await popup.getByRole('heading', { name: 'Defendant behaviour and lifestyle assessment' }).isVisible()
-    await popup.locator('#defendantBehaviour').fill(faker.lorem.sentence())
+    await popup.locator('[contenteditable]').fill(faker.lorem.sentence())
     await popup.locator('text=Save and continue').click()
 
     // - Risk analysis
@@ -76,15 +83,20 @@ test('Create a pre-sentence report', async ({ page }) => {
     await selectOption(popup, '#risk-to-public', 'Low risk')
     await selectOption(popup, '#risk-to-known-adults', 'High risk')
     await selectOption(popup, '#risk-to-staff', 'Low risk')
-    await popup.locator('#risk-predictors').fill(faker.lorem.sentence())
-    await popup.locator('#risk-riskAndHarmFactors').fill(faker.lorem.sentence())
+    for (const textarea of await popup.locator('[contenteditable]').elementHandles()) {
+        await textarea.click()
+        await popup.keyboard.type(faker.lorem.sentence())
+    }
     await popup.locator('text=Save and continue').click()
 
     // - Sentencing proposal
     await popup.getByRole('heading', { name: 'Sentencing proposal' }).isVisible()
-    await popup.locator('#proposedSentence').fill(faker.lorem.sentence())
-    await popup.locator('#proposedSentenceRationale').fill(faker.lorem.sentence())
-    await popup.locator('#alternativeSentencingOptions').fill(faker.lorem.sentence())
+    for (const textarea of await popup.locator('[contenteditable]').elementHandles()) {
+        if (await textarea.isVisible()) {
+            await textarea.click()
+            await popup.keyboard.type(faker.lorem.sentence())
+        }
+    }
     await popup.locator('#custodial-sentence-not-threshold').click()
     await popup.locator('text=Save and continue').click()
 
