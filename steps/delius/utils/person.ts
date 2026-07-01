b392e7d9f318de7f108a6a26d4a6f1077cf0b42e
Andrew Larcombe <andrew.larcombe@digital.justice.gov.uk>
2026-07-01 15:43:13 +0100

    Adds cro, ethnicity and enables data creation for car

--- steps/delius/utils/person.ts
+++ steps/delius/utils/person.ts
@@ -6,6 +6,8 @@ export interface Person {
     sex: string
     dob: Date
     pnc?: string
+    ethnicity?: string
+    croNumber?: string
 }
 
 const sexOf = (str: string): SexType => {
@@ -23,18 +25,31 @@ export function deliusPerson(person?: Partial<Person>): Person {
     const lastName = person?.lastName ? person.lastName : faker.person.lastName(sex)
     const dob = person?.dob ? person.dob : faker.date.birthdate({ min: 18, max: 69, mode: 'age' })
     const pnc = person?.pnc ? person.pnc : createPnc(dob)
+    const ethnicity = person?.ethnicity ? person.ethnicity : "W1"
+    const croNumber = person?.croNumber ? person.croNumber : createCroNumber()
     return {
         firstName,
         lastName,
         sex: sex[0].toUpperCase() + sex.substring(1),
         dob,
         pnc,
+        ethnicity,
+        croNumber
     }
 }
 
 export function createPnc(date: Date): string {
     const chars = 'ZABCDEFGHJKLMNPQRTUVWXY'
     const year = date.getFullYear().toString()
+    const randomNumber = faker.number.int({ min: 0, max: 9999999 })
+    const padded = randomNumber.toString().padStart(7, '0')
+    const character = chars.charAt(+(year.substring(2) + padded) % 23)
+    return year + '/' + padded + character
+}
+
+export function createCroNumber(): string {
+    const chars = 'ZABCDEFGHJKLMNPQRTUVWXY'
+    const year = faker.number.int({ min: 39, max: 95 }).toString()
     const randomNumber = faker.number.int({ min: 0, max: 9999999 })
     const padded = randomNumber.toString().padStart(7, '0')
     const character = chars.charAt(+(year.substring(2) + padded) % 23)
