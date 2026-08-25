402873f9de86481c405818e1e51ff731de9e0cc5
Wendy Moyo <wendy.moyo@justice.gov.uk>
2026-08-25 12:49:02 +0100

    Test fix for search button (#1109)
    fix

--- steps/manage-a-supervision/application.ts
+++ steps/manage-a-supervision/application.ts
@@ -1,9 +1,13 @@
 import { expect, Page } from '@playwright/test'
+import { doUntil } from '../delius/utils/refresh'
 
 export async function searchPersonInMPoP(page: Page, crn: string, heading?: ReturnType<Page['locator']>) {
     await page.getByRole('link', { name: 'Search' }).click()
     await page.getByLabel('Find a person on probation').fill(crn)
-    await expect(page.locator('#search-results-container')).toContainText(crn)
+    await doUntil(
+        () => page.getByRole('button', { name: 'Search' }).click(),
+        () => expect(page.locator('#search-results-container')).toContainText(crn)
+    )
 
     await page.locator(`[href$="${crn}"]`).first().click()
 
