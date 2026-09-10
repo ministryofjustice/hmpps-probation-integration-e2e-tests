61af12bf9268623475b227d52c274c07d55cf90e
moyow-moj <wendy.moyo@justice.gov.uk>
2026-09-10 13:30:21 +0100

    fixes

--- steps/manage-a-supervision/application.ts
+++ steps/manage-a-supervision/application.ts
@@ -3,14 +3,12 @@ import { doUntil } from '../delius/utils/refresh'
 
 export async function searchPersonInMPoP(page: Page, crn: string, heading?: ReturnType<Page['locator']>) {
     await page.getByRole('link', { name: 'Search' }).click()
-    await page.getByLabel('Find a person on probation').fill(crn)
     await doUntil(
-        () => page.getByRole('button', { name: 'Search' }).click(),
+        // Leaving this commented out as it's not confirmed whether the Search button will be removed
+        // () => page.getByRole('button', { name: 'Search' }).click(),
+        () => page.getByLabel('Find a person on probation').fill(crn),
         () => expect(page.locator('#search-results-container')).toContainText(crn)
     )
-
-    // Leaving this commented out as it's not confirmed whether the Search button will be removed
-    // await expect(page.locator('#search-results-container')).toContainText(crn)
 
     await page.locator(`[href$="${crn}"]`).first().click()
 
