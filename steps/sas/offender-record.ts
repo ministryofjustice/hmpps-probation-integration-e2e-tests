10344f1424c6c4fd48eb95825f891ee47029fdf1
Sean Wright <sean.wright@justice.gov.uk>
2026-09-16 10:10:27 +0100

    fix: fix case-list.soec.ts test
    Signed-off-by: Sean Wright <sean.wright@justice.gov.uk>

--- steps/sas/offender-record.ts
+++ steps/sas/offender-record.ts
@@ -3,5 +3,5 @@ import { type Page } from '@playwright/test'
 export const searchForPerson = async (page: Page, crn: string) => {
     await page.locator('#searchTerm').fill(crn)
     await page.getByRole('button', { name: /Apply filters/ }).click()
-    await page.getByRole('link', { name: 'Unknown' }).click()
+    await page.locator('a[href^="/cases/"]').click()
 }
